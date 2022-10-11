8667f838f55ef0072543ec1727e1267de47277f1
Lukasz Selwa <lukasz.selwa@swmansion.com>
2022-10-11 22:48:33 +0200

    Change Char to char

--- src/sernick/Tokenizer/Dfa/IDfa.cs
+++ src/sernick/Tokenizer/Dfa/IDfa.cs
@@ -2,7 +2,7 @@ namespace sernick.Tokenizer.Dfa;
 
 public interface IDfa<TState>
 {
-    TState Transition(TState state, Char atom);
+    TState Transition(TState state, char atom);
     bool IsAccepting(TState state);
     bool IsDead(TState state);
     TState Start { get; }
