ade523c87ce6afb7637b35043a66cb1d8f7e453b
Lukasz Selwa <lukasz.selwa@swmansion.com>
2022-10-12 10:15:56 +0200

    Add Start and End to Token

--- src/sernick/Tokenizer/Token.cs
+++ src/sernick/Tokenizer/Token.cs
@@ -1,12 +1,18 @@
+using sernick.Input;
+
 namespace sernick.Tokenizer;
 
 public class Token<TCat>
 {
-    public Token(TCat category, String text)
+    public Token(TCat category, string text, ILocation start, ILocation end)
     {
         Category = category;
         Text = text;
+        Start = start;
+        End = end;
     }
     public TCat Category { get; }
     public String Text { get; }
+    public ILocation Start { get; }
+    public ILocation End { get; }
 }
