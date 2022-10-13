0aac3ee9369f9cbf344c45aff77349479541a11b
Lukasz Selwa <lukasz.selwa@swmansion.com>
2022-10-13 11:13:02 +0200

    Fix formatting in tests

--- test/sernickTest/Tokenizer/Lexer/Lexer.cs
+++ test/sernickTest/Tokenizer/Lexer/Lexer.cs
@@ -1,10 +1,10 @@
 namespace sernickTest.Tokenizer.Lexer;
 
-public 
-class
-Lexer   
-{  
-    [Fact]   public void  LexerIsTruth(   )  {
+public class Lexer
+{
+    [Fact]
+    public void LexerIsTruth()
+    {
         Assert.True(true);
     }
 }
