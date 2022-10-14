09097c616ffb853a6ae8bd61d18a3a9ab9a7acdb
Władysław Raczek <wsw.raczek@gmail.com>
2022-10-14 14:26:39 +0200

    Modify priorities

--- src/sernick/Grammar/GrammarCategory.cs
+++ src/sernick/Grammar/GrammarCategory.cs
@@ -3,59 +3,59 @@ namespace sernick.Grammar
     public abstract class GrammarCategory
     {
         // 1 -- lowest priority
+        // Keep the priority high for categories which are more general
+        // Example: priority of a category which describes "var" keyword
+        // should be LOWER than of a category which describes variable identifiers
+        // because we want to say that "myvariableName" is a correct variable name,
+        // and not a mistake in keyword
         public Int16 Priority;
 
     }
 
-    public class CommentCategory : GrammarCategory
+    public class BraceCategory : GrammarCategory
     {
         public new Int16 Priority = 1;
     }
 
-    public class BraceCategory : GrammarCategory
+    public class LineDelimiterCategory : GrammarCategory
     {
         public new Int16 Priority = 2;
     }
 
-    public class LineDelimiterCategory : GrammarCategory
+    public class ColonCategory : GrammarCategory
     {
         public new Int16 Priority = 3;
     }
 
-    public class ColonCategory : GrammarCategory
+    public class WhitespaceCategory : GrammarCategory
     {
         public new Int16 Priority = 4;
     }
 
-    public class KeywordCategory : GrammarCategory
-    {
-        public new Int16 Priority = 8;
-    }
-
-    public class TypeIdentifierCategory : GrammarCategory
-    {
-        public new Int16 Priority = 4;
-    }
-    public class VariableIdentifierCategory : GrammarCategory
-    {
-        public new Int16 Priority = 4;
-    }
-
-    public class OperatorCategory : GrammarCategory
+    public class LiteralsCategory : GrammarCategory
     {
         public new Int16 Priority = 5;
     }
 
-    public class WhitespaceCategory : GrammarCategory
+    public class OperatorCategory : GrammarCategory
     {
         public new Int16 Priority = 6;
     }
-
-
-    public class LiteralsCategory : GrammarCategory
+    public class KeywordCategory : GrammarCategory
     {
         public new Int16 Priority = 7;
     }
 
-
+    public class TypeIdentifierCategory : GrammarCategory
+    {
+        public new Int16 Priority = 8;
+    }
+    public class VariableIdentifierCategory : GrammarCategory
+    {
+        public new Int16 Priority = 9;
+    }
+    public class CommentCategory : GrammarCategory
+    {
+        public new Int16 Priority = 10;
+    }
 }
