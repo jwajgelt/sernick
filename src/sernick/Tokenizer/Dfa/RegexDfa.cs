605132a57cecbb671048bc803e7be0de143abc91
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-10-15 18:07:39 +0200

    onelining again

--- src/sernick/Tokenizer/Dfa/RegexDfa.cs
+++ src/sernick/Tokenizer/Dfa/RegexDfa.cs
@@ -12,10 +12,7 @@ public sealed class RegexDfa : IDfa<Regex>
 
     public bool Accepts(Regex state) => state.ContainsEpsilon();
 
-    public bool IsDead(Regex state)
-    {
-        return state.Equals(Regex.Empty);
-    }
+    public bool IsDead(Regex state) => state.Equals(Regex.Empty);
 
     public Regex Transition(Regex state, char atom) => state.Derivative(atom);
 }
