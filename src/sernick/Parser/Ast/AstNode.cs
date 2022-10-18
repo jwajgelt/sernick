ff3365951167bf68df027f91ef24cbd4527a8526
Demian Banakh <dembanakh01@gmail.com>
2022-10-18 15:28:57 +0200

    [AST] Add `AstNode` base class declaration

---
+++ src/sernick/Parser/Ast/AstNode.cs
@@ -0,0 +1,9 @@
+namespace sernick.Parser.Ast;
+
+/// <summary>
+/// Base class for all types of nodes that can appear in AST (Abstract Syntax Tree)
+/// </summary>
+public abstract class AstNode
+{
+
+}
