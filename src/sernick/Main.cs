742c44e44c1375d31d1840f3f24a237bdf5cce29
Demian Banakh <dembanakh01@gmail.com>
2022-10-18 15:53:11 +0200

    [Frontend] Add `main()` placeholder

--- src/sernick/Main.cs
+++ src/sernick/Main.cs
@@ -3,3 +3,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("sernickTest")]
 
 Console.WriteLine("Hello, World!");
+// TODO: implement compiler's entry point here
+// "example.ser".ReadFile()
---
+++ src/sernick/Utility/FileUtility.cs
@@ -0,0 +1,11 @@
+using sernick.Input;
+
+namespace sernick.Utility;
+
+public static class FileUtility
+{
+    public static IInput ReadFile(this string fileName)
+    {
+        throw new NotImplementedException();
+    }
+}
