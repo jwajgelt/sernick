0dc041aeffea8f33bad687b8617b781fb5e1d7fe
Władysław Raczek <wsw.raczek@gmail.com>
2022-10-20 12:37:07 +0200

    Begin writing some code

--- src/sernick/Grammar/Dfa/GrammarAnalysis.cs
+++ src/sernick/Grammar/Dfa/GrammarAnalysis.cs
@@ -1,15 +1,45 @@
 namespace sernick.Grammar.Dfa;
 
-public static class GrammarAnalysis
+public static class GrammarAnalysis<TSymbol> where TSymbol : notnull
 {
     /// <summary>
     /// Compute the set NULLABLE - all symbols, from which epsilon can be derived in grammar
     /// </summary>
-    public static IReadOnlyCollection<TSymbol> Nullable<TSymbol, TDfaState>(
+    public static IReadOnlyCollection<TSymbol> Nullable<TDfaState>(
         this DfaGrammar<TSymbol, TDfaState> grammar)
         where TSymbol : IEquatable<TSymbol>
     {
-        throw new NotImplementedException();
+        List<TSymbol> Nullable = new List<TSymbol>();
+        // Use Tuple, so we remember which automata does every state come from
+        Queue<Tuple<TSymbol, TDfaState>> Q = new Queue<Tuple<TSymbol, TDfaState>>();
+        Dictionary<TSymbol, Queue<TDfaState>> ConditionalQueues = new Dictionary<TSymbol, Queue<TDfaState>>();
+
+        foreach (var symbol in grammar.Productions.Keys)
+        {
+            foreach (var acceptingState in grammar.Productions[symbol].AcceptingStates)
+            {
+                Q.Append(Tuple.Create(symbol, acceptingState));
+            }
+        }
+
+        while (Q.Count != 0)
+        {
+            Tuple<TSymbol, TDfaState> tuple = Q.Dequeue();
+            TSymbol symbol = tuple.Item1;
+            TDfaState state = tuple.Item2;
+            foreach (var transitionEdge in grammar.Productions[symbol].GetTransitionsTo(state))
+            {
+                var fromState = transitionEdge.From;
+                if (Nullable.Contains(transitionEdge.Atom))
+                {
+                    Nullable.Add(transitionEdge.Atom);
+                }
+
+            }
+
+        }
+
+
     }
 
     /// <summary>
