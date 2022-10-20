5fc66e20fdbaaa508ffb9c7d1701929ae89aa449
Piotrek <piotr-kaliciak@wp.pl>
2022-10-20 20:26:42 +0200

    Add LexicalError

---
+++ src/sernick/Tokenizer/Lexer/LexicalError.cs
@@ -0,0 +1,23 @@
+﻿using sernick.Diagnostics;
+using sernick.Input;
+
+namespace sernick.Tokenizer.Lexer;
+
+public class LexicalError : IDiagnosticItem
+{
+    public LexicalError(ILocation start, ILocation end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    private ILocation Start;
+    private ILocation End;
+    
+    public new string ToString()
+    {
+        return $"Lexical Error starting at: {Start.ToString()} and ending at: {End.ToString()}";
+    }
+    
+    public DiagnosticItemSeverity Severity => DiagnosticItemSeverity.Error;
+}
