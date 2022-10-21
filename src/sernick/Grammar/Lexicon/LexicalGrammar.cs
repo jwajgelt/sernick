3d2e51dfbd0f7fa888ec79e294a66f28f10516f2
Piotrek <piotr-kaliciak@wp.pl>
2022-10-21 16:07:53 +0200

    Implement FakeDiagnostics and test some example programs against Lexer

--- src/sernick/Grammar/Lexicon/LexicalGrammar.cs
+++ src/sernick/Grammar/Lexicon/LexicalGrammar.cs
@@ -66,7 +66,7 @@ public class LexicalGrammar
     {
         ["colon"] = ":"
     });
-    
+
     private readonly LexicalGrammarEntry comma = new(new CommaCategory(), new CategoryItems
     {
         ["comma"] = ","
