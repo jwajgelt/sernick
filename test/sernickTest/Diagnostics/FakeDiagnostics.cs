8a7e76f74714e5aec22384125a3d9a8d06bb3a89
Piotrek <piotr-kaliciak@wp.pl>
2022-10-22 17:17:02 +0200

    Correct FakeDiagnostics

--- test/sernickTest/Diagnostics/FakeDiagnostics.cs
+++ test/sernickTest/Diagnostics/FakeDiagnostics.cs
@@ -6,6 +6,6 @@ public class FakeDiagnostics : IDiagnostics
 {
     private readonly List<IDiagnosticItem> _items = new();
     public IEnumerable<IDiagnosticItem> DiagnosticItems => _items;
-    public bool DidErrorOccur => _items.Any();
+    public bool DidErrorOccur => _items.Any(item => item.Severity == DiagnosticItemSeverity.Error);
     public void Report(IDiagnosticItem diagnosticItem) => _items.Add(diagnosticItem);
 }
