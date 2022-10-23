dd6eee0ab67782446a81822aecbe1cf33ce008da
Demian Banakh <dembanakh01@gmail.com>
2022-10-23 21:35:35 +0200

    [Lint] Adjust linter severity for naming-style (#80)
    * [Lint] Make some naming-style rules merge-blocking
    
    * [Lint] Fix new linter errors

--- src/sernick/Parser/Ast/FlowControlStatements.cs
+++ src/sernick/Parser/Ast/FlowControlStatements.cs
@@ -2,11 +2,11 @@ namespace sernick.Parser.Ast;
 
 public sealed record ContinueStatement : FlowControlStatement { }
 
-public sealed record ReturnStatement(Expression returnValue) : FlowControlStatement;
+public sealed record ReturnStatement(Expression ReturnValue) : FlowControlStatement;
 
 public sealed record BreakStatement : FlowControlStatement { }
 
-public sealed record IfStatement(Expression condition,
-    CodeBlock ifBlock, CodeBlock? elseBlock) : FlowControlStatement;
+public sealed record IfStatement(Expression Condition,
+    CodeBlock IfBlock, CodeBlock? ElseBlock) : FlowControlStatement;
 
-public sealed record LoopStatement(CodeBlock inner) : FlowControlStatement;
+public sealed record LoopStatement(CodeBlock Inner) : FlowControlStatement;
--- src/sernick/Parser/Ast/SimpleValues.cs
+++ src/sernick/Parser/Ast/SimpleValues.cs
@@ -1,11 +1,11 @@
 namespace sernick.Parser.Ast;
 
-public sealed record VariableValue(Identifier identifier) : SimpleValue;
+public sealed record VariableValue(Identifier Identifier) : SimpleValue;
 
-public sealed record ConstValue(Identifier identifier) : SimpleValue;
+public sealed record ConstValue(Identifier Identifier) : SimpleValue;
 
 public abstract record LiteralValue : SimpleValue { }
 
-public sealed record BoolLiteralValue(bool value) : LiteralValue;
+public sealed record BoolLiteralValue(bool Value) : LiteralValue;
 
-public sealed record IntLiteralValue(int value) : LiteralValue;
+public sealed record IntLiteralValue(int Value) : LiteralValue;
