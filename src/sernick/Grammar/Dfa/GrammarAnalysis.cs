jwajgelt/sernick

commit 53cce9a7e8495fd787433bd3539c24b38c7bf298

Implement GrammarAnalysis.Nullable (#69)
Closes #55

This implements Nullable, using algorithm from lecture two

src/sernick/Grammar/Dfa/GrammarAnalysis.cs
@@ -4,14 +4,65 @@ using Utility;
 
 public static class GrammarAnalysis
 {
+
     /// <summary>
     /// Compute the set NULLABLE - all symbols, from which epsilon can be derived in grammar
     /// </summary>
     public static IReadOnlyCollection<TSymbol> Nullable<TSymbol, TDfaState>(
         this DfaGrammar<TSymbol, TDfaState> grammar)
-        where TSymbol : IEquatable<TSymbol>
+         where TSymbol : IEquatable<TSymbol>
+         where TDfaState : IEquatable<TDfaState>
     {
-        throw new NotImplementedException();
+        var nullable = new List<TSymbol>();
+        // Use ValueTuple here and below, so we remember which automata does every state come from
+        var queue = new Queue<ValueTuple<TSymbol, TDfaState>>();
+        var conditionalQueuesForSymbols = grammar.Productions.Keys.ToDictionary(symbol => symbol, _ => new Queue<ValueTuple<TSymbol, TDfaState>>());
+
+        foreach (var symbol in grammar.Productions.Keys)
+        {
+            foreach (var acceptingState in grammar.Productions[symbol].AcceptingStates)
+            {
+                queue.Enqueue((symbol, acceptingState));
+            }
+        }
+
+        while (queue.Count != 0)
+        {
+            var (currentSymbolWhichDeterminesAutomata, currentState) = queue.Dequeue();
+
+            var currentAutomata = grammar.Productions[currentSymbolWhichDeterminesAutomata];
+
+            // If we've encountered a start symbol for DFA => add all states from "conditional set" for "symbol" to Q
+            // and mark current symbol as nullable
+            if (currentAutomata.Start.Equals(currentState))
+            {
+                foreach (var (symbolWhichDeterminesAutomata, stateForThatSymbol) in conditionalQueuesForSymbols[currentSymbolWhichDeterminesAutomata])
+                {
+
+                    queue.Enqueue((symbolWhichDeterminesAutomata, stateForThatSymbol));
+                }
+
+                nullable.Add(currentSymbolWhichDeterminesAutomata);
+                conditionalQueuesForSymbols[currentSymbolWhichDeterminesAutomata].Clear();
+            }
+
+            foreach (var transitionEdge in grammar.Productions[currentSymbolWhichDeterminesAutomata].GetTransitionsTo(currentState))
+            {
+                var fromState = transitionEdge.From;
+                var atom = transitionEdge.Atom;
+                if (nullable.Contains(atom))
+                {
+                    queue.Enqueue((currentSymbolWhichDeterminesAutomata, fromState));
+                }
+                else
+                {
+                    // we need to remember that "fromState" is a state for a specific automata (one determined by symbolFromGrammar)
+                    conditionalQueuesForSymbols[atom].Enqueue((currentSymbolWhichDeterminesAutomata, fromState));
+                }
+            }
+        }
+
+        return nullable;
     }
 
     /// <summary>

test/sernickTest/Grammar/Dfa/TestGrammarAnalysis.cs
@@ -1,5 +1,4 @@
 namespace sernickTest.Grammar.Dfa;
-
 using sernick.Common.Dfa;
 using sernick.Grammar.Dfa;
 using Tokenizer.Lexer.Helpers;

commit ada9ff93786e96a3531874123692ec05360d5ce4

[Grammar][Fast-follow] Fix bug from PR #69 (#82)
There was one bug in Nullable, introduced in #69 which we missed during review -- if some symbols appear only on right-hand side of productions in grammar, we didn't ever create a new conditional queues for them, but we tried to access these queues like they exist (so we had a "missing key in dictionary" exception).

There also was another bug -- Nullable was not handling loops in automatas correctly. I fixed that too, by adding a used set

This PR fixes two mentioned bugs, also uncommenting all tests for grammar so #81 is now part of this PR

src/sernick/Grammar/Dfa/GrammarAnalysis.cs
@@ -13,7 +13,8 @@ public static class GrammarAnalysis
          where TSymbol : IEquatable<TSymbol>
          where TDfaState : IEquatable<TDfaState>
     {
-        var nullable = new List<TSymbol>();
+        var nullable = new HashSet<TSymbol>();
+        var used = new HashSet<ValueTuple<TSymbol, TDfaState>>(); // to help us in case of loops in automatas
         // Use ValueTuple here and below, so we remember which automata does every state come from
         var queue = new Queue<ValueTuple<TSymbol, TDfaState>>();
         var conditionalQueuesForSymbols = grammar.Productions.Keys.ToDictionary(symbol => symbol, _ => new Queue<ValueTuple<TSymbol, TDfaState>>());
@@ -29,7 +30,6 @@ public static class GrammarAnalysis
         while (queue.Count != 0)
         {
             var (currentSymbolWhichDeterminesAutomata, currentState) = queue.Dequeue();
-
             var currentAutomata = grammar.Productions[currentSymbolWhichDeterminesAutomata];
 
             // If we've encountered a start symbol for DFA => add all states from "conditional set" for "symbol" to Q
@@ -38,8 +38,12 @@ public static class GrammarAnalysis
             {
                 foreach (var (symbolWhichDeterminesAutomata, stateForThatSymbol) in conditionalQueuesForSymbols[currentSymbolWhichDeterminesAutomata])
                 {
-
-                    queue.Enqueue((symbolWhichDeterminesAutomata, stateForThatSymbol));
+                    var tupleToAdd = (symbolWhichDeterminesAutomata, stateForThatSymbol);
+                    if (!used.Contains(tupleToAdd))
+                    {
+                        queue.Enqueue(tupleToAdd);
+                        used.Add(tupleToAdd);
+                    }
                 }
 
                 nullable.Add(currentSymbolWhichDeterminesAutomata);
@@ -52,12 +56,17 @@ public static class GrammarAnalysis
                 var atom = transitionEdge.Atom;
                 if (nullable.Contains(atom))
                 {
-                    queue.Enqueue((currentSymbolWhichDeterminesAutomata, fromState));
+                    var tupleToAdd = (currentSymbolWhichDeterminesAutomata, fromState);
+                    if (!used.Contains(tupleToAdd))
+                    {
+                        queue.Enqueue(tupleToAdd);
+                        used.Add(tupleToAdd);
+                    }
                 }
                 else
                 {
                     // we need to remember that "fromState" is a state for a specific automata (one determined by symbolFromGrammar)
-                    conditionalQueuesForSymbols[atom].Enqueue((currentSymbolWhichDeterminesAutomata, fromState));
+                    conditionalQueuesForSymbols.GetOrAddEmpty(atom).Enqueue((currentSymbolWhichDeterminesAutomata, fromState));
                 }
             }
         }

test/sernickTest/Grammar/Dfa/TestGrammarAnalysis.cs
@@ -8,7 +8,7 @@ public class TestGrammarAnalysis
     // A -> eps
     // B -> b
     // C -> c|eps
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void NullableSimple()
     {
         var transitions1 = new Dictionary<(int, char), int> { };
@@ -56,7 +56,7 @@ public class TestGrammarAnalysis
     // S -> ABA
     // A -> eps
     // B -> AA
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void NullableConditional()
     {
         var transitions1 = new Dictionary<(int, char), int>
@@ -103,7 +103,7 @@ public class TestGrammarAnalysis
     // A -> eps
     // B -> b
     // S -> AAbB
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void FirstSimple()
     {
         var transitions1 = new Dictionary<(int, char), int> { };
@@ -153,7 +153,7 @@ public class TestGrammarAnalysis
     // B -> C
     // C -> A
     // S -> B
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void FirstClosure()
     {
         var transitions1 = new Dictionary<(int, char), int>
@@ -212,7 +212,7 @@ public class TestGrammarAnalysis
     // B -> eps
     // C -> eps
     // S -> CABC
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void FollowSimple()
     {
         var transitions1 = new Dictionary<(int, char), int>
@@ -267,7 +267,7 @@ public class TestGrammarAnalysis
     // D -> eps
     // E -> eps
     // S -> A(BB|CC)A(D|E) (with 2 accepting states)
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void FollowPaths()
     {
         var transitions1 = new Dictionary<(int, char), int> { };
@@ -323,7 +323,7 @@ public class TestGrammarAnalysis
     // C -> BA
     // D -> CA|B
     // S -> A(C|B)D
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void TestMixed()
     {
         var transitions1 = new Dictionary<(int, char), int>
@@ -402,7 +402,7 @@ public class TestGrammarAnalysis
     //       ^        B
     //       A        v
     //      ( ) <-B- ( ) -A-> [ ] 
-    [Fact(Skip = "GrammarAnalysis methods not implemented")]
+    [Fact]
     public void TestLoop()
     {
         var transitions1 = new Dictionary<(int, char), int> { };