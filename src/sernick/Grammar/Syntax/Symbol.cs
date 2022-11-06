635c5f65fbe0d1afe314f2932885ccee6239aeee
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-11-06 11:08:37 +0100

    Fix Symbol Equals

--- src/sernick/Grammar/Syntax/Symbol.cs
+++ src/sernick/Grammar/Syntax/Symbol.cs
@@ -4,9 +4,18 @@ using sernick.Tokenizer;
 
 public abstract record Symbol;
 
-public record Terminal(Token<LexicalGrammarCategoryType> Inner) : Symbol;
+public sealed record Terminal(Token<LexicalGrammarCategoryType> Inner) : Symbol, IEquatable<Terminal>
+{
+    public bool Equals(Terminal? other) =>
+        (other != null) &&
+        (Inner.Category == other.Inner.Category) &&
+        (Inner.Text == other.Inner.Text);
 
-public record NonTerminal(NonTerminalSymbol Inner) : Symbol;
+    public override int GetHashCode() =>
+        HashCode.Combine(Inner.Category, Inner.Text);
+}
+
+public sealed record NonTerminal(NonTerminalSymbol Inner) : Symbol;
 
 public enum NonTerminalSymbol
 {
