0674502bf9143a40b97730c50a6a739eb06365ee
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-11-06 11:43:27 +0100

    add some symbol boilerplate

--- src/sernick/Grammar/Syntax/SernickGrammar.cs
+++ src/sernick/Grammar/Syntax/SernickGrammar.cs
@@ -1,18 +1,45 @@
 namespace sernick.Grammar.Syntax;
+using sernick.Tokenizer;
+using sernick.Grammar.Lexicon;
+using sernick.Input;
 using sernick.Common.Regex;
+
+using Cat = Lexicon.LexicalGrammarCategoryType;
+
+sealed record PlaceholderLocation : ILocation {};
 
 public static class SernickGrammarProvider
 {
     public static Grammar<Symbol> createSernickGrammar()
     {
         var productions = new List<Production<Symbol>>();
+        PlaceholderLocation I = new PlaceholderLocation();
 
         Symbol program = new NonTerminal(NonTerminalSymbol.Program);
         Symbol expression = new NonTerminal(NonTerminalSymbol.Expression);
+        Symbol codeBlock = new NonTerminal(NonTerminalSymbol.CodeBlock);
+        Symbol ifStatement = new NonTerminal(NonTerminalSymbol.IfStatement);
+        Symbol loopStatement = new NonTerminal(NonTerminalSymbol.LoopStatement);
+        Symbol variableDeclaration = new NonTerminal(NonTerminalSymbol.VariableDeclaration);
+
+        Symbol semicolon = new Terminal(new Token<Cat>(Cat.Semicolon, ";", I, I));
+
+        var reg_expression = Regex<Symbol>.Atom(expression);
+        var reg_semicolon = Regex<Symbol>.Atom(semicolon);
 
         productions.Add(new Production<Symbol>(
             program,
-            Regex<Symbol>.Atom(expression)
+            reg_expression
+        ));
+
+        productions.Add(new Production<Symbol>(
+            expression,
+            Regex<Symbol>.Concat((reg_expression, reg_semicolon, reg_expression))
+        ));
+
+        productions.Add(new Production<Symbol>(
+            expression,
+            Regex<Symbol>.Concat((reg_expression, reg_semicolon, reg_expression))
         ));
 
         return new Grammar<Symbol>(
