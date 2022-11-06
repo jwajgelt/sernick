3237e4c3576ce71decaee841b035dc9cd562a727
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-11-06 15:30:30 +0100

    Adding some productions

--- src/sernick/Grammar/Syntax/SernickGrammar.cs
+++ src/sernick/Grammar/Syntax/SernickGrammar.cs
@@ -1,7 +1,5 @@
 namespace sernick.Grammar.Syntax;
-using sernick.Common.Regex;
-using sernick.Input;
-using sernick.Tokenizer;
+
 using LexicalCategory = Lexicon.LexicalGrammarCategoryType;
 using Regex = Common.Regex.Regex<Symbol>;
 
@@ -11,26 +9,64 @@ public static class SernickGrammar
     {
         var productions = new List<Production<Symbol>>();
 
+        // Symbols of our grammar
+
+        // Non-terminal
         var program = new NonTerminal(NonTerminalSymbol.Program);
         var expression = new NonTerminal(NonTerminalSymbol.Expression);
         var codeBlock = new NonTerminal(NonTerminalSymbol.CodeBlock);
         var ifStatement = new NonTerminal(NonTerminalSymbol.IfStatement);
         var loopStatement = new NonTerminal(NonTerminalSymbol.LoopStatement);
-        var varDeclaration = new NonTerminal(NonTerminalSymbol.VariableDeclaration);
+        _ = new NonTerminal(NonTerminalSymbol.VariableDeclaration);
 
+        // Terminal
         var semicolon = new Terminal(LexicalCategory.Semicolon, ";");
+        var bracesOpen = new Terminal(LexicalCategory.BracesAndParentheses, "{");
+        var bracesClose = new Terminal(LexicalCategory.BracesAndParentheses, "}");
+        var parenthesesOpen = new Terminal(LexicalCategory.BracesAndParentheses, "(");
+        var parenthesesClose = new Terminal(LexicalCategory.BracesAndParentheses, ")");
+        var loopKeyword = new Terminal(LexicalCategory.Keywords, "loop");
+        var ifKeyword = new Terminal(LexicalCategory.Keywords, "if");
 
-        var reg_expression = Regex<Symbol>.Atom(expression);
-        var reg_semicolon = Regex<Symbol>.Atom(semicolon);
+        // Atomic regular expressions representing symbols
+
+        // For non-terminal
+        var regExpression = Regex.Atom(expression);
+        var regCodeBlock = Regex.Atom(codeBlock);
+
+        // For terminal
+        var regSemicolon = Regex.Atom(semicolon);
+        var regBracesOpen = Regex.Atom(bracesOpen);
+        var regBracesClose = Regex.Atom(bracesClose);
+        var regParenthesesOpen = Regex.Atom(parenthesesOpen);
+        var regParenthesesClose = Regex.Atom(parenthesesClose);
+        var regLoopKeyword = Regex.Atom(loopKeyword);
+        var regIfKeyword = Regex.Atom(ifKeyword);
 
         productions.Add(new Production<Symbol>(
             program,
-            reg_expression
+            regExpression
         ));
 
         productions.Add(new Production<Symbol>(
             expression,
-            Regex.Concat(new Regex[] { reg_expression, reg_semicolon, reg_expression })
+            Regex.Concat(new Regex[] { regExpression, regSemicolon, regExpression })
+        ));
+
+        productions.Add(new Production<Symbol>(
+            codeBlock,
+            Regex.Concat(new Regex[] { regBracesOpen, regExpression, regBracesClose })
+        ));
+
+        productions.Add(new Production<Symbol>(
+            loopStatement,
+            Regex.Concat(new Regex[] { regLoopKeyword, regCodeBlock })
+        ));
+
+        // Need to add optional "elseCodeBlock" 
+        productions.Add(new Production<Symbol>(
+            ifStatement,
+            Regex.Concat(new Regex[] { regIfKeyword, regParenthesesOpen, regExpression, regParenthesesClose, regCodeBlock, regCodeBlock })
         ));
 
         return new Grammar<Symbol>(
