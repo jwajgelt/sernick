6f13d351ccf2c7bb6583eb9f2aaaff20493a74dc
Krzysztof Ziobro <86822080+krzysztofziobro@users.noreply.github.com>
2022-11-06 15:46:01 +0100

    Create initial `Symbol` class for sernick grammar (#105)

---
+++ src/sernick/Grammar/Syntax/Symbol.cs
@@ -0,0 +1,37 @@
+namespace sernick.Grammar.Syntax;
+using LexicalCategory = Lexicon.LexicalGrammarCategoryType;
+
+public abstract record Symbol;
+
+public sealed record Terminal(LexicalCategory Category, string Text) : Symbol
+{
+    public bool Equals(Terminal? other) => Category switch
+    {
+        LexicalCategory.BracesAndParentheses or LexicalCategory.Operators or LexicalCategory.Keywords => (Category, Text) == (other?.Category, other?.Text),
+        _ => (Category == other?.Category)
+    };
+
+    public override int GetHashCode() => Category switch
+    {
+        LexicalCategory.BracesAndParentheses or LexicalCategory.Operators or LexicalCategory.Keywords => HashCode.Combine(Category, Text),
+        _ => Category.GetHashCode()
+    };
+}
+
+public sealed record NonTerminal(NonTerminalSymbol Inner) : Symbol;
+
+public enum NonTerminalSymbol
+{
+    Program,
+    Expression,
+    CodeBlock,
+    IfStatement,
+    LoopStatement,
+    VariableDeclaration,
+    ConstDeclaration,
+    FunctionDefinition,
+    FunctionCall,
+    Assignment,
+    ExpSummand,
+    ExpFactor
+}
