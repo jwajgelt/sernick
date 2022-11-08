b092247bde8a35d782af0c4cc5cf949e373d02d8
Juliusz Wajgelt <jwajgelt@gmail.com>
2022-11-08 14:40:49 +0100

    `AstNode.From(ParseTree)` method signature

--- src/sernick/Parser/Ast/AstNode.cs
+++ src/sernick/Parser/Ast/AstNode.cs
@@ -1,9 +1,23 @@
 namespace sernick.Parser.Ast;
 
+using Grammar.Syntax;
+using ParseTree;
+
 /// <summary>
 /// Base class for all types of nodes that can appear in AST (Abstract Syntax Tree)
 /// </summary>
-public abstract record AstNode { }
+public abstract record AstNode
+{
+    
+    /// <summary>
+    /// Constructs the Abstract Syntax Tree from the given parse tree
+    /// <param name="parseTree">Parse tree to generate the AST from</param>
+    /// </summary>
+    public static AstNode From(IParseTree<Symbol> parseTree)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 /// <summary>
 /// Class representing identifiers
