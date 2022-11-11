76478dad145ec49165e52407f932b657184ea2f0
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-11-11 13:41:49 +0100

    Implement reporting UndeclaredIdentifierError

---
+++ src/sernick/Ast/Analysis/UndeclaredIdentifierError.cs
@@ -0,0 +1,14 @@
+﻿namespace sernick.Ast.Analysis;
+
+using Diagnostics;
+using Nodes;
+
+public record UndeclaredIdentifierError(Identifier Identifier) : IDiagnosticItem
+{
+    public DiagnosticItemSeverity Severity => DiagnosticItemSeverity.Error;
+    
+    public override string ToString()
+    {
+        return $"Undeclared identifier: {Identifier}";
+    }
+}
