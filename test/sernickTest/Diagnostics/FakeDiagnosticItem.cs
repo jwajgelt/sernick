d751d85d197d3bae8d3dd2cde5876db4a3d03cbd
Szymon Salabura <ssalabura@protonmail.com>
2022-11-11 20:36:40 +0100

    Remove Equals from FakeDiagnosticItem

--- test/sernickTest/Diagnostics/FakeDiagnosticItem.cs
+++ test/sernickTest/Diagnostics/FakeDiagnosticItem.cs
@@ -4,5 +4,4 @@ using sernick.Diagnostics;
 
 public record FakeDiagnosticItem(DiagnosticItemSeverity Severity) : IDiagnosticItem
 {
-    public bool Equals(IDiagnosticItem? other) => other is FakeDiagnosticItem && other.Severity == Severity && other.ToString() == ToString();
 }
