d1c467ef40d384cbccf418510880358af7862de5
Lukasz Selwa <lukasz.selwa@swmansion.com>
2022-11-15 11:18:20 +0100

    Refactor tests

--- test/sernickTest/Ast/Nodes/ConversionTest.cs
+++ test/sernickTest/Ast/Nodes/ConversionTest.cs
@@ -8,163 +8,164 @@ using sernick.Grammar.Syntax;
 
 public class ConversionTest
 {
+
+    private static FakeParseTreeNode Node(NonTerminalSymbol nonTerminal, params IFakeParseTree[] children)
+        => new(Symbol.Of(nonTerminal), children);
+
+    private static FakeParseTreeLeaf Leaf(LexicalGrammarCategory category, string text)
+        => new(Symbol.Of(category, text));
+    
     [Fact]
     public void EmptyExpression_Conversion()
     {
-        var parseTree = Fake.Node(NonTerminalSymbol.Program,
-            Fake.Node(NonTerminalSymbol.ExpressionSeq, Array.Empty<IFakeParseTree>())
+        var parseTree = Node(NonTerminalSymbol.Program,
+            Node(NonTerminalSymbol.ExpressionSeq, Array.Empty<IFakeParseTree>())
         ).Convert();
 
-        var unit = new UnitExpression((IFakeParseTree.Locations.End, IFakeParseTree.Locations.End));
-        Assert.Equal(unit, AstNode.From(parseTree));
+        Assert.True(AstNode.From(parseTree) is UnitExpression);
     }
 
     [Fact]
     public void BreakKeyword_Conversion()
     {
-        var input = Fake.Leaf(LexicalGrammarCategory.Keywords, "break").Convert();
-        var breakStatement = new BreakStatement(IFakeParseTree.Locations);
-        Assert.Equal(breakStatement, AstNode.From(input));
+        var input = Leaf(LexicalGrammarCategory.Keywords, "break").Convert();
+        Assert.True(AstNode.From(input) is BreakStatement);
     }
 
     [Fact]
     public void ContinueKeyword_Conversion()
     {
-        var input = Fake.Leaf(LexicalGrammarCategory.Keywords, "continue").Convert();
-        var continueStatement = new ContinueStatement(IFakeParseTree.Locations);
-        Assert.Equal(continueStatement, AstNode.From(input));
+        var input = Leaf(LexicalGrammarCategory.Keywords, "continue").Convert();
+        Assert.True(AstNode.From(input) is ContinueStatement);
     }
 
     [Fact]
     public void ReturnExpression_Conversion()
     {
-        var input = Fake.Node(NonTerminalSymbol.ReturnExpression,
-            Fake.Leaf(LexicalGrammarCategory.Keywords, "return")).Convert();
-        var returnStatement = new ReturnStatement(null, IFakeParseTree.Locations);
-        Assert.Equal(returnStatement, AstNode.From(input));
+        var input = Node(NonTerminalSymbol.ReturnExpression,
+            Leaf(LexicalGrammarCategory.Keywords, "return")).Convert();
+        Assert.True(AstNode.From(input) is ReturnStatement { ReturnValue: null });
     }
 
     [Fact]
     public void Bracketed_OpenedExpression_Conversion()
     {
         // { x }
-        var input = Fake.Node(NonTerminalSymbol.CodeBlock,
-            Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "{"),
-            Fake.Node(NonTerminalSymbol.ExpressionSeq,
-                Fake.Node(NonTerminalSymbol.OpenExpression,
-                    Fake.Node(NonTerminalSymbol.LogicalOperand,
-                        Fake.Node(NonTerminalSymbol.ComparisonOperand,
-                            Fake.Node(NonTerminalSymbol.ArithmeticOperand,
-                                Fake.Node(NonTerminalSymbol.SimpleExpression,
-                                    Fake.Leaf(LexicalGrammarCategory.VariableIdentifiers, "x")
+        var input = Node(NonTerminalSymbol.CodeBlock,
+            Leaf(LexicalGrammarCategory.BracesAndParentheses, "{"),
+            Node(NonTerminalSymbol.ExpressionSeq,
+                Node(NonTerminalSymbol.OpenExpression,
+                    Node(NonTerminalSymbol.LogicalOperand,
+                        Node(NonTerminalSymbol.ComparisonOperand,
+                            Node(NonTerminalSymbol.ArithmeticOperand,
+                                Node(NonTerminalSymbol.SimpleExpression,
+                                    Leaf(LexicalGrammarCategory.VariableIdentifiers, "x")
                                     )))))),
-            Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "}")
+            Leaf(LexicalGrammarCategory.BracesAndParentheses, "}")
             ).Convert();
-
-        var ast = (CodeBlock)AstNode.From(input);
-        var variableValue = new VariableValue(new Identifier("x", IFakeParseTree.Locations),
-            IFakeParseTree.Locations);
-
-        Assert.Equal(variableValue, ast.Inner);
+        
+        Assert.True(AstNode.From(input) is CodeBlock { Inner: VariableValue { Identifier.Name: "x" } });
     }
 
     [Fact]
     public void Bracketed_ClosedExpression_Conversion()
     {
         // { x; }
-        var input = Fake.Node(NonTerminalSymbol.CodeBlock,
-            Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "{"),
-            Fake.Node(NonTerminalSymbol.ExpressionSeq,
-                Fake.Node(NonTerminalSymbol.OpenExpression,
-                    Fake.Node(NonTerminalSymbol.LogicalOperand,
-                        Fake.Node(NonTerminalSymbol.ComparisonOperand,
-                            Fake.Node(NonTerminalSymbol.ArithmeticOperand,
-                                Fake.Node(NonTerminalSymbol.SimpleExpression,
-                                    Fake.Leaf(LexicalGrammarCategory.VariableIdentifiers, "x")
+        var input = Node(NonTerminalSymbol.CodeBlock,
+            Leaf(LexicalGrammarCategory.BracesAndParentheses, "{"),
+            Node(NonTerminalSymbol.ExpressionSeq,
+                Node(NonTerminalSymbol.OpenExpression,
+                    Node(NonTerminalSymbol.LogicalOperand,
+                        Node(NonTerminalSymbol.ComparisonOperand,
+                            Node(NonTerminalSymbol.ArithmeticOperand,
+                                Node(NonTerminalSymbol.SimpleExpression,
+                                    Leaf(LexicalGrammarCategory.VariableIdentifiers, "x")
                                 ))))),
-                Fake.Leaf(LexicalGrammarCategory.Semicolon, ";")
+                Leaf(LexicalGrammarCategory.Semicolon, ";")
                 ),
-            Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "}")
+            Leaf(LexicalGrammarCategory.BracesAndParentheses, "}")
         ).Convert();
-        var ast = (CodeBlock)AstNode.From(input);
-        var innerExpression = (ExpressionJoin)ast.Inner;
 
         // Converter should add Unit type after "x" so that the CodeBlock has the right type
-        var variableValue = new VariableValue(new Identifier("x", IFakeParseTree.Locations),
-            IFakeParseTree.Locations);
-        var unit = new UnitExpression((IFakeParseTree.Locations.End, IFakeParseTree.Locations.End));
-        Assert.Equal(variableValue, innerExpression.First);
-        Assert.Equal(unit, innerExpression.Second);
+        Assert.True(AstNode.From(input) is CodeBlock { Inner: ExpressionJoin
+        {
+            First: VariableValue { Identifier.Name: "x" },
+            Second: UnitExpression
+        }});
     }
 
     [Fact]
     public void ArithmeticExpression_Conversion()
     {
         // 1 + 2
-        var input = Fake.Node(NonTerminalSymbol.ComparisonOperand,
-            Fake.Node(NonTerminalSymbol.ArithmeticOperand,
-                Fake.Node(NonTerminalSymbol.SimpleExpression,
-                    Fake.Node(NonTerminalSymbol.LiteralValue,
-                        Fake.Leaf(LexicalGrammarCategory.Literals, "1")))),
-            Fake.Node(NonTerminalSymbol.ArithmeticOperator,
-                Fake.Leaf(LexicalGrammarCategory.Operators, "+")),
-            Fake.Node(NonTerminalSymbol.ArithmeticOperand,
-                Fake.Node(NonTerminalSymbol.SimpleExpression,
-                    Fake.Node(NonTerminalSymbol.LiteralValue,
-                        Fake.Leaf(LexicalGrammarCategory.Literals, "2"))))
+        var input = Node(NonTerminalSymbol.ComparisonOperand,
+            Node(NonTerminalSymbol.ArithmeticOperand,
+                Node(NonTerminalSymbol.SimpleExpression,
+                    Node(NonTerminalSymbol.LiteralValue,
+                        Leaf(LexicalGrammarCategory.Literals, "1")))),
+            Node(NonTerminalSymbol.ArithmeticOperator,
+                Leaf(LexicalGrammarCategory.Operators, "+")),
+            Node(NonTerminalSymbol.ArithmeticOperand,
+                Node(NonTerminalSymbol.SimpleExpression,
+                    Node(NonTerminalSymbol.LiteralValue,
+                        Leaf(LexicalGrammarCategory.Literals, "2"))))
         ).Convert();
-        var expression = new Infix(
-            new IntLiteralValue(1, IFakeParseTree.Locations),
-            new IntLiteralValue(2, IFakeParseTree.Locations),
-            Infix.Op.Plus
-            );
-        Assert.Equal(expression, AstNode.From(input));
+
+        Assert.True(AstNode.From(input) is Infix
+        {
+            Left: IntLiteralValue { Value: 1 },
+            Right:  IntLiteralValue { Value: 2 },
+            Operator: Infix.Op.Plus
+        });
     }
 
     [Fact]
     public void FunctionDeclaration_Conversion()
     {
         // fun foo(x: Int = 1) { x }
-        var input = Fake.Node(NonTerminalSymbol.FunctionDeclaration,
-            Fake.Leaf(LexicalGrammarCategory.Keywords, "fun"),
-            Fake.Leaf(LexicalGrammarCategory.VariableIdentifiers, "foo"),
-            Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "("),
-            Fake.Node(NonTerminalSymbol.FunctionParameters,
-                Fake.Node(NonTerminalSymbol.FunctionParameterWithDefaultValue,
-                    Fake.Leaf(LexicalGrammarCategory.VariableIdentifiers, "x"),
-                    Fake.Node(NonTerminalSymbol.TypeSpecification,
-                        Fake.Leaf(LexicalGrammarCategory.Colon, ","),
-                        Fake.Leaf(LexicalGrammarCategory.TypeIdentifiers, "Int")
+        var input = Node(NonTerminalSymbol.FunctionDeclaration,
+            Leaf(LexicalGrammarCategory.Keywords, "fun"),
+            Leaf(LexicalGrammarCategory.VariableIdentifiers, "foo"),
+            Leaf(LexicalGrammarCategory.BracesAndParentheses, "("),
+            Node(NonTerminalSymbol.FunctionParameters,
+                Node(NonTerminalSymbol.FunctionParameterWithDefaultValue,
+                    Leaf(LexicalGrammarCategory.VariableIdentifiers, "x"),
+                    Node(NonTerminalSymbol.TypeSpecification,
+                        Leaf(LexicalGrammarCategory.Colon, ":"),
+                        Leaf(LexicalGrammarCategory.TypeIdentifiers, "Int")
                     ),
-                    Fake.Leaf(LexicalGrammarCategory.Operators, "="),
-                    Fake.Node(NonTerminalSymbol.LiteralValue,
-                        Fake.Leaf(LexicalGrammarCategory.Literals, "1")
+                    Leaf(LexicalGrammarCategory.Operators, "="),
+                    Node(NonTerminalSymbol.LiteralValue,
+                        Leaf(LexicalGrammarCategory.Literals, "1")
                     )
                 )
             ),
-            Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, ")"),
-            Fake.Node(NonTerminalSymbol.CodeBlock,
-                Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "{"),
-                Fake.Node(NonTerminalSymbol.ExpressionSeq,
-                    Fake.Node(NonTerminalSymbol.SimpleExpression,
-                        Fake.Leaf(LexicalGrammarCategory.VariableIdentifiers, "x")
+            Leaf(LexicalGrammarCategory.BracesAndParentheses, ")"),
+            Node(NonTerminalSymbol.CodeBlock,
+                Leaf(LexicalGrammarCategory.BracesAndParentheses, "{"),
+                Node(NonTerminalSymbol.ExpressionSeq,
+                    Node(NonTerminalSymbol.SimpleExpression,
+                        Leaf(LexicalGrammarCategory.VariableIdentifiers, "x")
                     )
                 ),
-                Fake.Leaf(LexicalGrammarCategory.BracesAndParentheses, "}"))
+                Leaf(LexicalGrammarCategory.BracesAndParentheses, "}"))
         ).Convert();
-        var fun = (FunctionDefinition)AstNode.From(input);
+        var astNode = AstNode.From(input);
 
-        var xVar = new Identifier("x", IFakeParseTree.Locations);
-        var expectedParameter = new FunctionParameterDeclaration(xVar, new IntType(), new IntLiteralValue(1, IFakeParseTree.Locations), IFakeParseTree.Locations);
-        var expectedStatement = new VariableValue(xVar, IFakeParseTree.Locations);
-
-        Assert.Equal(new Identifier("foo", IFakeParseTree.Locations), fun.Name);
-        Assert.Single(fun.Parameters, expectedParameter);
-        Assert.Single(fun.Body.Children, expectedStatement);
+        Assert.True(astNode is FunctionDefinition
+        {
+            Name.Name: "foo",
+            ReturnType: UnitType,
+            Body.Inner: VariableValue { Identifier.Name: "x" }
+        });
+        
+        var funAstNode = (FunctionDefinition)astNode;
+        Assert.Single(funAstNode.Parameters);
+        Assert.True(funAstNode.Parameters.First() is
+        {
+            Name.Name: "x",
+            Type: IntType,
+            DefaultValue: IntLiteralValue { Value: 1 }
+        });
     }
-}
-internal static class Fake
-{
-    public static FakeParseTreeNode Node(NonTerminalSymbol nonTerminal, params IFakeParseTree[] children) => new(Symbol.Of(nonTerminal), children);
-    public static FakeParseTreeLeaf Leaf(LexicalGrammarCategory category, string text) => new(Symbol.Of(category, text));
 }
