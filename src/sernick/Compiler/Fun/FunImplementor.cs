81d40c7befe194dacce0e2a566eac0797502b669
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-11-15 22:18:19 +0100

    AddLocal fix

--- src/sernick/Compiler/Fun/FunImplementor.cs
+++ src/sernick/Compiler/Fun/FunImplementor.cs
@@ -1,8 +1,10 @@
 namespace sernick.Compiler.Fun;
 
+public abstract record FunVariable;
+
 public abstract class FunImplementor : FunCaller
 {
-    public void addLocal(bool usedElsewhere)
+    public void AddLocal(FunVariable variable, bool usedElsewhere)
     {
         throw new NotImplementedException();
     }
