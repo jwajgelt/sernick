fc81b211c0b9e6a3bf96a9fbd0841e664495de21
Krzysztof Ziobro <86822080+krzysztofziobro@users.noreply.github.com>
2022-11-16 16:05:42 +0100

    Update src/sernick/Compiler/Function/FunctionFactory.cs
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>

--- src/sernick/Compiler/Function/FunctionFactory.cs
+++ src/sernick/Compiler/Function/FunctionFactory.cs
@@ -4,7 +4,7 @@ public abstract record FunctionParam;
 
 public sealed class FunFactory
 {
-    public IFunctionContext MoreFun(IFunctionContext? parent, IReadOnlyCollection<FunctionParam> parameters, bool result)
+    public IFunctionContext MoreFun(IFunctionContext? parent, IReadOnlyCollection<FunctionParam> parameters, bool hasResult)
     {
         throw new NotImplementedException();
     }
