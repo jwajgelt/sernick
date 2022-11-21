6e4ce52bdb3c0f5796355cf2c2c789dd97fa4b66
Władysław Raczek <wsw.raczek@gmail.com>
2022-11-21 16:19:59 +0100

    Add a pendingNodes set and a few helper methods

--- src/sernick/Ast/Analysis/TypeChecking.cs
+++ src/sernick/Ast/Analysis/TypeChecking.cs
@@ -25,82 +25,144 @@ public sealed class TypeChecking
         /// and stored in partialExpressionTypes dictionary
         /// </summary>
         private readonly NameResolutionResult nameResolution;
-        private IDiagnostics _diagnostics;
+        private readonly IDiagnostics _diagnostics;
+        /// <summary>
+        /// Sometimes we would like to know the result for our ancestor,
+        /// so to avoid recalculation (visiting the same ancestor from multiple nodes)
+        /// we will have this helper object, containing type information for some AST nodes
+        /// </summary>
+        private TypeInformation partialResult;
+        /// <summary>
+        /// Our Type-checking Algorighm is a top-down postorder
+        /// But sometimes, nodes need to know information about some other nodes higher up the tree
+        /// And in case we need to know a type information about our direct ancestor -- that's a circular dependency
+        /// Which we have to uncover and report
+        /// For example, a case like this:
+        /// var x : Int = { 23; x = 23; } // here  a usage of x needs information about the type of x
+        /// </summary>
+        private readonly HashSet<AstNode> pendingNodes;
 
         public TypeCheckingAstVisitor(NameResolutionResult nameResolution, IDiagnostics diagnostics)
         {
             this.nameResolution = nameResolution;
             this._diagnostics = diagnostics;
+            this.partialResult = new TypeInformation();
+            this.pendingNodes = new HashSet<AstNode>();
         }
 
         protected override TypeInformation VisitAstNode(AstNode node, Unit _)
         {
+            pendingNodes.Add(node);
+
             // just visit recursively, bottom-up
             // for simple things, just visit them without recursion
             if (node.Children.Count() == 0)
             {
                 var emptyTypeInformation = new TypeInformation();
-                return node.Accept(this, Unit.I);
+                var result = node.Accept(this, Unit.I);
+                pendingNodes.Remove(node);
+                return result;
             }
 
             var childrenTypes = this.visitNodeChildren(node);
 
             // TODO what to do after we've visited node children
+            pendingNodes.Remove(node);
 
 
             return childrenTypes;
         }
 
+        public override TypeInformation VisitIdentifier(Identifier identifierNode, Unit _)
+        {
+            // no need to visit children, identifier nodes should have none
+            // var childrenTypes = this.visitNodeChildren(identifierNode);
+            try
+            {
+                var variableDeclarationNode = this.getIdentifierAsVariableDeclaration(identifierNode);
+                partialResult[identifierNode] = partialResult[variableDeclarationNode];
+                return new TypeInformation() { { identifierNode, partialResult[variableDeclarationNode] } };
+            }
+            finally { }
+
+            try
+            {
+                var functionDefinitionNode = this.getIdentifierAsFunctionDefinition(identifierNode);
+                partialResult[identifierNode] = partialResult[functionDefinitionNode];
+                return new TypeInformation() { { identifierNode, partialResult[functionDefinitionNode] } };
+            }
+            finally { }
+
+            // An identifier should always point to either function definition or variable definition
+            // So the code should never reach this line
+            // But just in case, let's simply return unit here
+            partialResult[identifierNode] = new UnitType();
+            return new TypeInformation() { { identifierNode, new UnitType() } };
+
+
+        }
+
         public override TypeInformation VisitVariableDeclaration(VariableDeclaration node, Unit _)
         {
             // all necessary checking should be performed in "Visit assignment", so just return Unit here
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
 
         }
 
         public override TypeInformation VisitFunctionParameterDeclaration(FunctionParameterDeclaration node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
         }
 
         public override TypeInformation VisitFunctionDefinition(FunctionDefinition node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
         }
 
         public override TypeInformation VisitCodeBlock(CodeBlock node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             // simply return what expression inside returns?
             var result = new TypeInformation(childrenTypes);
             result.Add(node, childrenTypes[node.Inner]);
+            pendingNodes.Remove(node);
             return result;
 
         }
 
         public override TypeInformation VisitExpressionJoin(ExpressionJoin node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
             result.Add(node, childrenTypes[node.Second]); // just return the last expressions' type
+            pendingNodes.Remove(node);
             return result;
         }
 
         public override TypeInformation VisitFunctionCall(FunctionCall functionCallNode, Unit _)
         {
+            pendingNodes.Add(functionCallNode);
             var childrenTypes = this.visitNodeChildren(functionCallNode);
 
             var functionDeclarationNode = nameResolution.CalledFunctionDeclarations[functionCallNode];
@@ -111,11 +173,13 @@ public sealed class TypeChecking
             if (inferredReturnType.ToString() != declaredReturnType.ToString())
             {
                 this._diagnostics.Report(new TypeCheckingError(declaredReturnType, inferredReturnType, functionCallNode.LocationRange.Start));
+                pendingNodes.Remove(functionCallNode);
                 return childrenTypes; // not sure here
             }
 
             var result = new TypeInformation(childrenTypes);
             result.Add(functionCallNode, new UnitType()); // function call returns a unit, not a function return type
+            pendingNodes.Remove(functionCallNode);
             return result;
 
         }
@@ -123,68 +187,80 @@ public sealed class TypeChecking
         public override TypeInformation VisitContinueStatement(ContinueStatement node, Unit _)
         {
             // TODO do we need to visit node.children here?
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
         }
 
         public override TypeInformation VisitReturnStatement(ReturnStatement node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
             // Return Value is in a subtree, so its type should be already calculated by now
             result.Add(node, childrenTypes[node.ReturnValue]);
+            pendingNodes.Remove(node);
             return result;
         }
 
         public override TypeInformation VisitBreakStatement(BreakStatement node, Unit _)
         {
             // TODO do we need to visit node.children here?
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
         }
 
 
         public override TypeInformation VisitIfStatement(IfStatement node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             // TODO
+            pendingNodes.Remove(node);
             return null;
         }
 
         public override TypeInformation VisitLoopStatement(LoopStatement node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             // TODO what if Loop contains a "return 12;"?
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
         }
 
 
-        public override TypeInformation VisitInfix(Infix infixNode, Unit _)
+        public override TypeInformation VisitInfix(Infix node, Unit _)
         {
-            var childrenTypes = this.visitNodeChildren(infixNode);
+            pendingNodes.Add(node);
+            var childrenTypes = this.visitNodeChildren(node);
 
-            var typeOfLeftOperand = childrenTypes[infixNode.LeftSide];
-            var typeOfRightOperand = childrenTypes[infixNode.RightSide];
+            var typeOfLeftOperand = childrenTypes[node.LeftSide];
+            var typeOfRightOperand = childrenTypes[node.RightSide];
 
             if (typeOfLeftOperand.ToString() != typeOfRightOperand.ToString())
             {
                 // TODO we probably should create a separate error class for operator type mismatch
-                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, infixNode.LocationRange.Start));
+                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, node.LocationRange.Start));
 
                 // TODO does it make sense to return anything here? maybe a Unit type? But it could propagate the error up the tree 
                 var result = new TypeInformation(childrenTypes);
-                result.Add(infixNode, new UnitType());
+                result.Add(node, new UnitType());
+                pendingNodes.Remove(node);
                 return result;
             }
             else
@@ -192,14 +268,14 @@ public sealed class TypeChecking
                 var commonType = typeOfLeftOperand;
 
                 // let's cover some special cases e.g. adding two bools or shirt-curcuiting two ints
-                switch (infixNode.Operator)
+                switch (node.Operator)
                 {
                     case Infix.Op.Plus:
                     case Infix.Op.Minus:
                         {
                         if(commonType.ToString() == new BoolType().ToString())
                             {
-                                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, infixNode.LocationRange.Start));
+                                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, node.LocationRange.Start));
                             }
                             break;
                         }
@@ -210,7 +286,7 @@ public sealed class TypeChecking
                         {
                             if(commonType.ToString() != new IntType().ToString())
                             {
-                                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, infixNode.LocationRange.Start));
+                                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, node.LocationRange.Start));
                             }
                             break;
                         }
@@ -219,7 +295,7 @@ public sealed class TypeChecking
                         {
                             if(commonType.ToString() != new BoolType().ToString())
                             {
-                                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, infixNode.LocationRange.Start));
+                                this._diagnostics.Report(new TypeCheckingError(typeOfLeftOperand, typeOfRightOperand, node.LocationRange.Start));
                             }
                             break;
                         }
@@ -229,13 +305,15 @@ public sealed class TypeChecking
                         }      
                 }
                 var result = new TypeInformation(childrenTypes);
-                result.Add(infixNode, commonType);
+                result.Add(node, commonType);
+                pendingNodes.Remove(node);
                 return result;
             }
         }
 
         public override TypeInformation VisitAssignment(Assignment node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var typeOfLeftSide = childrenTypes[node.Left];
@@ -248,11 +326,13 @@ public sealed class TypeChecking
             // Regardless of the error, let's return a Unit type for assignment and get more type checking information
             var result = new TypeInformation(childrenTypes);
             result.Add(node, new UnitType());
+            pendingNodes.Remove(node);
             return result;
         }
 
         public override TypeInformation VisitVariableValue(VariableValue node, Unit _)
         {
+            pendingNodes.Add(node);
             var childrenTypes = this.visitNodeChildren(node);
 
             var result = new TypeInformation(childrenTypes);
@@ -260,6 +340,7 @@ public sealed class TypeChecking
             var variableIdentifier = node.Identifier;
 
             result.Add(node, result[node.Identifier]);
+            pendingNodes.Remove(node);
             return result;
 
             //var variableDeclarationNode = this.nameResolution.UsedVariableDeclarations[node];
@@ -319,6 +400,18 @@ public sealed class TypeChecking
         /// <returns></returns>
         private TypeInformation visitNodeChildren(AstNode node)
         {
+            var nodeHasNoChildren = !node.Children.Any();
+            if(nodeHasNoChildren)
+            {
+                return new TypeInformation();
+            }
+
+            var avoidRecalculation = partialResult.ContainsKey(node);
+            if (avoidRecalculation)
+            {
+                return partialResult;
+            }
+
             return node.Children.Aggregate(new TypeInformation(),
                 (partialTypeInformation, childNode) =>
                 {
@@ -329,5 +422,38 @@ public sealed class TypeChecking
            );
         }
 
+        private VariableDeclaration getIdentifierAsVariableDeclaration(Identifier identifier)
+        {
+            foreach(var declarationPair in nameResolution.AssignedVariableDeclarations)
+            {
+                var assignment = declarationPair.Key;
+                var variableDeclaration = declarationPair.Value;
+
+                if(variableDeclaration.Name == identifier)
+                {
+                    return variableDeclaration;
+                }
+            }
+            throw new Exception();
+        }
+
+        private FunctionDefinition getIdentifierAsFunctionDefinition(Identifier identifier)
+        {
+            foreach (var declarationPair in nameResolution.CalledFunctionDeclarations)
+            {
+                var assignment = declarationPair.Key;
+                var functionDefinition = declarationPair.Value;
+
+                if (functionDefinition.Name == identifier)
+                {
+                    return functionDefinition;
+                }
+            }
+            throw new Exception();
+        }
+
     }
+
+
+    
 }
