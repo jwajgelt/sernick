43c086b45262bf8adbf40008c1c0b3f65fd3e0f9
Lukasz Selwa <lukasz.selwa@swmansion.com>
2022-11-27 19:29:16 +0100

    Fix lint

--- test/sernickTest/Compiler/Function/FunctionContextGenerateVariableAccessTest.cs
+++ test/sernickTest/Compiler/Function/FunctionContextGenerateVariableAccessTest.cs
@@ -13,9 +13,9 @@ public class FunctionContextGenerateVariableAccessTest
         var context = new FunctionContext(null, Array.Empty<IFunctionParam>(), false, 0);
         var variable = Var("x");
         context.AddLocal(variable, usedElsewhere: false);
-        
+
         var readCodeTree = context.GenerateVariableRead(variable);
-        
+
         Assert.IsType<RegisterRead>(readCodeTree);
     }
 
@@ -28,7 +28,7 @@ public class FunctionContextGenerateVariableAccessTest
         var value = new Constant(new RegisterValue(1));
 
         var writeCodeTree = context.GenerateVariableWrite(variable, value);
-        
+
         var registerWrite = Assert.IsType<RegisterWrite>(writeCodeTree);
         Assert.Equal(value, registerWrite.Value);
     }
@@ -39,37 +39,37 @@ public class FunctionContextGenerateVariableAccessTest
         var context = new FunctionContext(null, Array.Empty<IFunctionParam>(), false, 0);
         var variable = Var("x");
         context.AddLocal(variable, usedElsewhere: true);
-        
+
         var readCodeTree = context.GenerateVariableRead(variable);
 
         var expectedTree =
             new MemoryRead(
                 new BinaryOperationNode(
-                    BinaryOperation.Sub, 
-                    new RegisterRead(HardwareRegister.RBP), 
+                    BinaryOperation.Sub,
+                    new RegisterRead(HardwareRegister.RBP),
                     new Constant(new RegisterValue(8)))
                 );
         Assert.Equal(expectedTree, readCodeTree);
     }
-    
+
     [Fact]
     public void Generates_MemoryRead_for_FunctionParams()
     {
         Fun<UnitType>("foo").Parameter<IntType>("arg", out var arg);
-        var context = new FunctionContext(null, new []{ arg }, false, 0);
-        
+        var context = new FunctionContext(null, new[] { arg }, false, 0);
+
         var readCodeTree = context.GenerateVariableRead(arg);
 
         var expectedTree =
             new MemoryRead(
                 new BinaryOperationNode(
-                    BinaryOperation.Sub, 
-                    new RegisterRead(HardwareRegister.RBP), 
+                    BinaryOperation.Sub,
+                    new RegisterRead(HardwareRegister.RBP),
                     new Constant(new RegisterValue(-16)))
             );
         Assert.Equal(expectedTree, readCodeTree);
     }
-    
+
     [Fact]
     public void Generates_MemoryWrite_for_shared_Variable()
     {
@@ -77,20 +77,20 @@ public class FunctionContextGenerateVariableAccessTest
         var variable = Var("x");
         context.AddLocal(variable, usedElsewhere: true);
         var value = new Constant(new RegisterValue(1));
-        
+
         var readCodeTree = context.GenerateVariableWrite(variable, value);
 
         var expectedTree =
             new MemoryWrite(
                 new BinaryOperationNode(
-                    BinaryOperation.Sub, 
-                    new RegisterRead(HardwareRegister.RBP), 
+                    BinaryOperation.Sub,
+                    new RegisterRead(HardwareRegister.RBP),
                     new Constant(new RegisterValue(8))),
                 value
             );
         Assert.Equal(expectedTree, readCodeTree);
     }
-    
+
     [Fact]
     public void Throws_when_Variable_is_Undefined()
     {
@@ -119,7 +119,7 @@ public class FunctionContextGenerateVariableAccessTest
         parentContext.AddLocal(variable, true);
         parentContext.SetDisplayAddress(displayAddress);
         var context = new FunctionContext(parentContext, Array.Empty<IFunctionParam>(), false, 1);
-        
+
         var readCodeTree = context.GenerateVariableRead(variable);
 
         var expectedTree = new MemoryRead(
@@ -136,7 +136,7 @@ public class FunctionContextGenerateVariableAccessTest
             );
         Assert.Equal(expectedTree, readCodeTree);
     }
-    
+
     [Fact]
     public void Generates_Indirect_MemoryWrite_outer_Variable()
     {
@@ -148,7 +148,7 @@ public class FunctionContextGenerateVariableAccessTest
         parentContext.AddLocal(variable, true);
         parentContext.SetDisplayAddress(displayAddress);
         var context = new FunctionContext(parentContext, Array.Empty<IFunctionParam>(), false, 1);
-        
+
         var readCodeTree = context.GenerateVariableWrite(variable, value);
 
         var expectedTree = new MemoryWrite(
