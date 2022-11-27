41b52c1a2456d812efdb322d4a2067498c2dcb40
Demian Banakh <dembanakh01@gmail.com>
2022-11-27 18:21:08 +0100

    Use NameResolutionResultBuilder in FunctionContextMap tests (#176)

--- test/sernickTest/Ast/Analysis/FunctionContextMap/FunctionContextMapProcessorTest.cs
+++ test/sernickTest/Ast/Analysis/FunctionContextMap/FunctionContextMapProcessorTest.cs
@@ -1,15 +1,12 @@
 namespace sernickTest.Ast.Analysis.FunctionContextMap;
 
-using Castle.Core;
 using Compiler.Function.Helpers;
 using Moq;
 using sernick.Ast;
 using sernick.Ast.Analysis.FunctionContextMap;
-using sernick.Ast.Analysis.NameResolution;
-using sernick.Ast.Nodes;
 using sernick.Compiler.Function;
-using sernick.Utility;
 using static Helpers.AstNodesExtensions;
+using static Helpers.NameResolutionResultBuilder;
 
 public class FunctionContextMapProcessorTest
 {
@@ -31,8 +28,7 @@ public class FunctionContextMapProcessorTest
             .Setup(f => f.CreateFunction(mainContext, new[] { declA }, false))
             .Returns(fContext);
 
-        var nameResolution = NameResolution(
-            calledFunctions: new[] { (call, declaration) });
+        var nameResolution = NameResolution().WithCalls((call, declaration));
         var contextMap = FunctionContextMapProcessor.Process(tree, nameResolution, contextFactory.Object);
 
         Assert.Same(fContext, contextMap.Implementations[declaration]);
@@ -102,13 +98,12 @@ public class FunctionContextMapProcessorTest
             .Setup(f => f.CreateFunction(fContext, Array.Empty<IFunctionParam>(), false))
             .Returns(gContext);
 
-        var nameResolution = NameResolution(
-            usedVariables: new[]
-            {
-                (valueA, paramA as Declaration),
-                (valueY, declY as Declaration)
-            },
-            assignedVariables: new[] { (assignment, declW) });
+        var nameResolution = NameResolution()
+            .WithVars(
+                (valueA, paramA),
+                (valueY, declY))
+            .WithAssigns(
+                (assignment, declW));
         FunctionContextMapProcessor.Process(tree, nameResolution, contextFactory.Object);
 
         Assert.True(fContext.Locals[paramA]);
@@ -148,13 +143,12 @@ public class FunctionContextMapProcessorTest
             .Setup(f => f.CreateFunction(mainContext, new[] { paramA }, false))
             .Returns(functionContext);
 
-        var nameResolution = NameResolution(
-            usedVariables: new[]
-            {
-                (valueA, paramA as Declaration),
-                (valueX, declX as Declaration)
-            },
-            calledFunctions: new[] { (call, funDeclaration) });
+        var nameResolution = NameResolution()
+            .WithVars(
+                (valueA, paramA),
+                (valueX, declX))
+            .WithCalls(
+                (call, funDeclaration));
         FunctionContextMapProcessor.Process(tree, nameResolution, contextFactory.Object);
 
         Assert.False(functionContext.Locals[paramA]);
@@ -202,8 +196,8 @@ public class FunctionContextMapProcessorTest
             .Setup(f => f.CreateFunction(fContext, Array.Empty<IFunctionParam>(), true))
             .Returns(hContext);
 
-        var nameResolution = NameResolution(
-            usedVariables: new[] { (valueX, declX as Declaration) });
+        var nameResolution = NameResolution()
+            .WithVars((valueX, declX));
         FunctionContextMapProcessor.Process(tree, nameResolution, contextFactory.Object);
 
         Assert.False(gContext.Locals[declXinG]);
@@ -246,32 +240,12 @@ public class FunctionContextMapProcessorTest
             .Setup(f => f.CreateFunction(gContext, Array.Empty<IFunctionParam>(), false))
             .Returns(hContext);
 
-        var nameResolution = NameResolution(
-            usedVariables: new[] { (valueX, declX as Declaration) });
+        var nameResolution = NameResolution()
+            .WithVars((valueX, declX));
         FunctionContextMapProcessor.Process(tree, nameResolution, contextFactory.Object);
 
         Assert.True(fContext.Locals[declX]);
         Assert.Single(fContext.Locals);
-    }
-
-    private static NameResolutionResult NameResolution(
-        IEnumerable<(VariableValue, Declaration)>? usedVariables = null,
-        IEnumerable<(Assignment, VariableDeclaration)>? assignedVariables = null,
-        IEnumerable<(FunctionCall, FunctionDefinition)>? calledFunctions = null)
-    {
-        var usedVariablesDict = usedVariables.OrEmpty()
-            .ToDictionary(pair => pair.Item1, pair => pair.Item2,
-                ReferenceEqualityComparer<VariableValue>.Instance);
-
-        var assignedVariablesDict = assignedVariables.OrEmpty()
-            .ToDictionary(pair => pair.Item1, pair => pair.Item2,
-                ReferenceEqualityComparer<Assignment>.Instance);
-
-        var calledFunctionsDict = calledFunctions.OrEmpty()
-            .ToDictionary(pair => pair.Item1, pair => pair.Item2,
-                ReferenceEqualityComparer<FunctionCall>.Instance);
-
-        return new NameResolutionResult(usedVariablesDict, assignedVariablesDict, calledFunctionsDict);
     }
 
     private static Mock<IFunctionFactory> SetupFunctionFactory(out IFunctionContext mainContext)
