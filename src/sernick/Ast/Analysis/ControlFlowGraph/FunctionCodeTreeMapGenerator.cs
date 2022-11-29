5680a66fe1dd8c4dab3062bae4910edef9550588
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-11-29 20:12:38 +0100

    Replace IDictionary with IImmutableDictionary in FunctionCodeTreeMapGenerator

--- src/sernick/Ast/Analysis/ControlFlowGraph/FunctionCodeTreeMapGenerator.cs
+++ src/sernick/Ast/Analysis/ControlFlowGraph/FunctionCodeTreeMapGenerator.cs
@@ -1,18 +1,19 @@
 namespace sernick.Ast.Analysis.ControlFlowGraph;
 
+using System.Collections.Immutable;
 using Nodes;
 using sernick.ControlFlowGraph.CodeTree;
 using Utility;
 
 public static class FunctionCodeTreeMapGenerator
 {
-    public static IDictionary<FunctionDefinition, CodeTreeRoot> Process(AstNode root, Func<FunctionDefinition, CodeTreeRoot> unravel)
+    public static IReadOnlyDictionary<FunctionDefinition, CodeTreeRoot> Process(AstNode root, Func<FunctionDefinition, CodeTreeRoot> unravel)
     {
         var visitor = new GeneratorVisitor(unravel);
         return root.Accept(visitor, Unit.I);
     }
 
-    private sealed class GeneratorVisitor : AstVisitor<IDictionary<FunctionDefinition, CodeTreeRoot>, Unit>
+    private sealed class GeneratorVisitor : AstVisitor<IImmutableDictionary<FunctionDefinition, CodeTreeRoot>, Unit>
     {
         private readonly Func<FunctionDefinition, CodeTreeRoot> _unravel;
 
@@ -20,26 +21,18 @@ public static class FunctionCodeTreeMapGenerator
         {
             _unravel = unravel;
         }
-        protected override IDictionary<FunctionDefinition, CodeTreeRoot> VisitAstNode(AstNode node, Unit param)
+
+        protected override IImmutableDictionary<FunctionDefinition, CodeTreeRoot> VisitAstNode(AstNode node, Unit param)
         {
-            IDictionary<FunctionDefinition, CodeTreeRoot> emptyDict = new Dictionary<FunctionDefinition, CodeTreeRoot>();
+            IImmutableDictionary<FunctionDefinition, CodeTreeRoot> emptyDict = ImmutableDictionary<FunctionDefinition, CodeTreeRoot>.Empty;
             return node.Children.Aggregate(emptyDict, (dict, child) =>
-                Merge(dict, child.Accept(this, Unit.I)));
+                dict.AddRange(child.Accept(this, Unit.I)));
         }
 
-        public override IDictionary<FunctionDefinition, CodeTreeRoot> VisitFunctionDefinition(FunctionDefinition node, Unit param)
+        public override IImmutableDictionary<FunctionDefinition, CodeTreeRoot> VisitFunctionDefinition(FunctionDefinition node, Unit param)
         {
             var result = VisitAstNode(node, Unit.I);
-            result[node] = _unravel(node);
-            return result;
-        }
-
-        /// <summary>
-        /// Adds all elements of the smaller dict to the other
-        /// </summary>
-        private static IDictionary<K, V> Merge<K, V>(IDictionary<K, V> dict, IDictionary<K, V> other)
-        {
-            return dict.Count > other.Count ? dict.MergeWith(other) : other.MergeWith(dict);
+            return result.Add(node, _unravel(node));
         }
     }
 }
