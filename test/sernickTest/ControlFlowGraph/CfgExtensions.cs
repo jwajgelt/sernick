4f19a49e0ae207acdb5877e0060af505db817315
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-11-29 18:04:26 +0100

    Add IsIsomorphicWith stub

---
+++ test/sernickTest/ControlFlowGraph/CfgExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace sernickTest.ControlFlowGraph;
+
+using sernick.ControlFlowGraph.CodeTree;
+
+public static class CfgExtensions
+{
+    /// <summary>
+    /// Determines whether CSTs starting in given CodeTreeRoots are isomorphic
+    /// </summary>
+    public static bool IsIsomorphicWith(this CodeTreeRoot root, CodeTreeRoot other)
+    {
+        throw new NotImplementedException();
+    }
+}
