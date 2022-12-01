edc02e17668f839aed8642fbe1bd45224511a31b
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-12-01 16:24:06 +0100

    wip register labeling

--- test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
+++ test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
@@ -4,9 +4,43 @@ using sernick.ControlFlowGraph.CodeTree;
 
 public sealed class CfgIsomorphismComparer : IEqualityComparer<CodeTreeRoot>
 {
-    public bool Equals(CodeTreeRoot? x, CodeTreeRoot? y)
+    private bool areEqual(
+        CodeTreeRoot? x,
+        CodeTreeRoot? y, 
+        Dictionary<Register, int> regToLabel, 
+        HashSet<CodeTreeRoot> visitSet
+        )
     {
-        throw new NotImplementedException();
+        if(x is null && y is null)
+        {
+            return true;
+        }
+
+        if(x is null || y is null)
+        {
+            return false;
+        }
+
+        if(visitSet.Contains(x) != visitSet.Contains(y))
+        {
+            return false;
+        }
+
+        if(visitSet.Contains(x))    // ???
+        {
+            return true;
+        }
+
+        visitSet.Add(x);
+        visitSet.Add(y);
+    }
+
+    public bool Equals(CodeTreeRoot? x, CodeTreeRoot? y)
+    {   
+        var regToLabel = new Dictionary<Register, Register>();
+        var visitSet = new HashSet<CodeTreeRoot>();
+
+        return false;
     }
 
     public int GetHashCode(CodeTreeRoot obj)
