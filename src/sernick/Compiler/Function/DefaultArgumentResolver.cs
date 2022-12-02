26bea0ee28249977257e257968a81db34e53dfee
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-12-02 07:59:41 +0100

    Adding mechanism for extracting default values

---
+++ src/sernick/Compiler/Function/DefaultArgumentResolver.cs
@@ -0,0 +1,27 @@
+namespace sernick.Compiler.Function;
+
+using ControlFlowGraph.CodeTree;
+
+public static class DefaultArgumentResolver
+{
+    public static CodeTreeValueNode GetDefaultValue(IFunctionParam param)
+    {
+        var defaultValue = param.TryGetDefaultValue();
+        if(defaultValue == null)
+        {
+            throw new Exception("Requested a default value, but there is none.");
+        }
+
+        if(defaultValue is int intValue)
+        {
+            return intValue;
+        }
+
+        if(defaultValue is bool boolValue)
+        {
+            return boolValue ? 1 : 0;
+        }
+
+        throw new Exception("Default value of unsupported type.");
+    }
+}
