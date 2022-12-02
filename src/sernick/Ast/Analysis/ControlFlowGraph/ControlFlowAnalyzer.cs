391d798e89d1f7cf5d5ab70dac35654c42445e4f
Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
2022-12-02 16:21:44 +0100

    [Frontend] Add AST to CFG conversion (#180)
    * Add throws after every step
    
    * Add FunctionCodeTreeGenerator
    
    * Change ControlFlowAnalyzer interface
    
    * Add tests
    
    * Replace IDictionary with IImmutableDictionary in FunctionCodeTreeMapGenerator

--- src/sernick/Ast/Analysis/ControlFlowGraph/ControlFlowAnalyzer.cs
+++ src/sernick/Ast/Analysis/ControlFlowGraph/ControlFlowAnalyzer.cs
@@ -1,11 +1,18 @@
 namespace sernick.Ast.Analysis.ControlFlowGraph;
 
+using Compiler.Function;
+using FunctionContextMap;
+using NameResolution;
 using Nodes;
 using sernick.ControlFlowGraph.CodeTree;
 
 public static class ControlFlowAnalyzer
 {
-    public static IReadOnlyList<CodeTreeNode> UnravelControlFlow(AstNode root, Func<AstNode, CodeTreeNode?, IReadOnlyList<CodeTreeNode>> pullOutSideEffects)
+    public static CodeTreeRoot UnravelControlFlow(
+        AstNode root,
+        NameResolutionResult nameResolution,
+        FunctionContextMap contextMap,
+        Func<AstNode, NameResolutionResult, IFunctionContext, IReadOnlyList<SingleExitNode>> pullOutSideEffects)
     {
         throw new NotImplementedException();
     }
