7f46916e7623b2a7708bdcaf95a3b39984914d79
Krzysztof Ziobro <86822080+krzysztofziobro@users.noreply.github.com>
2022-12-05 22:50:50 +0100

    CFG Isomorphism (#190)
    * wip register labeling
    
    * cfg labeling 'interface'
    
    * parameters fix
    
    * iso comparer
    
    * very simple tests
    
    * fmt
    
    * Update test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
    
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>
    
    * Update test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
    
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>
    
    * Update test/sernickTest/ControlFlowGraph/CfgIsomorphismComparerTest.cs
    
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>
    
    * review
    
    * fmt
    
    * cycle tests
    
    * mutable single exit
    
    * linter
    
    * Update src/sernick/ControlFlowGraph/CodeTree/CodeTreeRoot.cs
    
    Co-authored-by: Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>
    
    * try get value
    
    * Update test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
    
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>
    
    * better comment
    
    * Update test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
    
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>
    
    Co-authored-by: Demian Banakh <dembanakh01@gmail.com>
    Co-authored-by: Aleksander Katan <56294622+aleksanderkatan@users.noreply.github.com>

--- test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
+++ test/sernickTest/ControlFlowGraph/CfgIsomorphismComparer.cs
@@ -2,11 +2,135 @@ namespace sernickTest.ControlFlowGraph;
 
 using sernick.ControlFlowGraph.CodeTree;
 
+public class CfgComparison
+{
+    private readonly Dictionary<Register, int> _registerLabels;
+    private int _labelsCount;
+    private readonly Dictionary<CodeTreeRoot, CodeTreeRoot> _visitMap;
+    public bool AreEqual { get; }
+
+    public CfgComparison(CodeTreeRoot? x, CodeTreeRoot? y)
+    {
+        _registerLabels = new Dictionary<Register, int>(ReferenceEqualityComparer.Instance);
+        _labelsCount = 0;
+        _visitMap = new Dictionary<CodeTreeRoot, CodeTreeRoot>(ReferenceEqualityComparer.Instance);
+        AreEqual = Same(x, y);
+    }
+
+    private bool Same(CodeTreeRoot? x, CodeTreeRoot? y)
+    {
+        if (x is null && y is null)
+        {
+            return true;
+        }
+
+        if (x is null || y is null)
+        {
+            return false;
+        }
+
+        if (_visitMap.TryGetValue(x, out var visitX) && !ReferenceEquals(visitX, y))
+        {
+            return false;
+        }
+
+        if (_visitMap.TryGetValue(y, out var visitY) && !ReferenceEquals(visitY, x))
+        {
+            return false;
+        }
+
+        if (visitX is not null || visitY is not null)
+        {
+            return true;
+        }
+
+        _visitMap[x] = y;
+        _visitMap[y] = x;
+
+        return (x, y) switch
+        {
+            (SingleExitNode xSingle, SingleExitNode ySingle) =>
+                Same(xSingle.Operations, ySingle.Operations) &&
+                Same(xSingle.NextTree, ySingle.NextTree),
+            (ConditionalJumpNode xConditional, ConditionalJumpNode yConditional) =>
+                Same(xConditional.ConditionEvaluation, yConditional.ConditionEvaluation) &&
+                Same(xConditional.TrueCase, yConditional.TrueCase) &&
+                Same(xConditional.FalseCase, yConditional.FalseCase),
+            _ => false,
+        };
+    }
+
+    private bool Same(IReadOnlyList<CodeTreeNode> x, IReadOnlyList<CodeTreeNode> y)
+    {
+        return x.Count == y.Count && x.Zip(y).All(p => Same(p.First, p.Second));
+    }
+
+    private bool Same(CodeTreeNode x, CodeTreeNode y)
+    {
+        return (x, y) switch
+        {
+            (BinaryOperationNode xBinOp, BinaryOperationNode yBinOp) =>
+                xBinOp.Operation.Equals(yBinOp.Operation) &&
+                Same(xBinOp.Right, yBinOp.Right) &&
+                Same(xBinOp.Left, yBinOp.Left),
+
+            (UnaryOperationNode xUnOp, UnaryOperationNode yUnOp) =>
+                xUnOp.Operation.Equals(yUnOp.Operation) &&
+                Same(xUnOp.Operand, yUnOp.Operand),
+
+            (MemoryRead xMemRd, MemoryRead yMemRd) =>
+                Same(xMemRd.MemoryLocation, yMemRd.MemoryLocation),
+
+            (MemoryWrite xMemWrt, MemoryWrite yMemWrt) =>
+                Same(xMemWrt.MemoryLocation, yMemWrt.MemoryLocation) &&
+                Same(xMemWrt.Value, yMemWrt.Value),
+
+            (RegisterRead xRegRd, RegisterRead yRegRd) =>
+                Same(xRegRd.Register, yRegRd.Register),
+
+            (RegisterWrite xRegWrt, RegisterWrite yRegWrt) =>
+                Same(xRegWrt.Register, yRegWrt.Register) &&
+                Same(xRegWrt.Value, yRegWrt.Value),
+
+            _ => x.Equals(y),
+        };
+    }
+
+    private bool Same(Register x, Register y)
+    {
+        if (x is HardwareRegister && y is HardwareRegister)
+        {
+            return x.Equals(y);
+        }
+
+        if (x is HardwareRegister || y is HardwareRegister)
+        {
+            return false;
+        }
+
+        if (_registerLabels.ContainsKey(x) && _registerLabels.ContainsKey(y))
+        {
+            return _registerLabels[x] == _registerLabels[y];
+        }
+
+        if (_registerLabels.ContainsKey(x) || _registerLabels.ContainsKey(y))
+        {
+            return false;
+        }
+
+        _registerLabels[x] = _labelsCount;
+        _registerLabels[y] = _labelsCount;
+        _labelsCount += 1;
+        return true;
+    }
+}
+
 public sealed class CfgIsomorphismComparer : IEqualityComparer<CodeTreeRoot>
 {
     public bool Equals(CodeTreeRoot? x, CodeTreeRoot? y)
     {
-        throw new NotImplementedException();
+        var cfgComparison = new CfgComparison(x, y);
+        return cfgComparison.AreEqual;
     }
 
     public int GetHashCode(CodeTreeRoot obj)
---
+++ test/sernickTest/ControlFlowGraph/CfgIsomorphismComparerTest.cs
@@ -0,0 +1,116 @@
+namespace sernickTest.ControlFlowGraph;
+
+using sernick.ControlFlowGraph.CodeTree;
+using static sernick.ControlFlowGraph.CodeTree.CodeTreeExtensions;
+
+public class CfgIsomorphismComparerTest
+{
+    [Fact]
+    public void HardwareRegisterDiff()
+    {
+        var raxRead = Reg(HardwareRegister.RAX).Read();
+        var rbxRead = Reg(HardwareRegister.RBX).Read();
+
+        var cfgA = new SingleExitNode(null, new List<CodeTreeNode> { raxRead });
+        var cfgB = new SingleExitNode(null, new List<CodeTreeNode> { rbxRead });
+
+        Assert.NotEqual(cfgA, cfgB, new CfgIsomorphismComparer());
+    }
+
+    [Fact]
+    public void SingleRegisterCompare()
+    {
+        var aRegRead = Reg(new Register()).Read();
+        var bRegRead = Reg(new Register()).Read();
+
+        var cfgA = new SingleExitNode(null, new List<CodeTreeNode> { aRegRead });
+        var cfgB = new SingleExitNode(null, new List<CodeTreeNode> { bRegRead });
+
+        Assert.Equal(cfgA, cfgB, new CfgIsomorphismComparer());
+    }
+
+    [Fact]
+    public void TwoMatchingRegisterCompare()
+    {
+        var aReg = Reg(new Register());
+        var bReg = Reg(new Register());
+
+        var aRegRead = aReg.Read();
+        var bRegRead = bReg.Read();
+
+        var aRegWrite = aReg.Write(5);
+        var bRegWrite = bReg.Write(5);
+
+        var cfgA = new SingleExitNode(null, new List<CodeTreeNode> { aRegRead, aRegWrite });
+        var cfgB = new SingleExitNode(null, new List<CodeTreeNode> { bRegRead, bRegWrite });
+
+        Assert.Equal(cfgA, cfgB, new CfgIsomorphismComparer());
+    }
+
+    [Fact]
+    public void TwoNonMatchingRegisterCompare()
+    {
+        var aReg = Reg(new Register());
+        var cReg = Reg(new Register());
+
+        var bReg = Reg(new Register());
+
+        var aRegRead = aReg.Read();
+        var bRegRead = bReg.Read();
+
+        var cRegWrite = cReg.Write(5);
+        var bRegWrite = bReg.Write(5);
+
+        var cfgA = new SingleExitNode(null, new List<CodeTreeNode> { aRegRead, cRegWrite });
+        var cfgB = new SingleExitNode(null, new List<CodeTreeNode> { bRegRead, bRegWrite });
+
+        Assert.NotEqual(cfgA, cfgB, new CfgIsomorphismComparer());
+    }
+
+    [Fact]
+    public void EquivalentCycles()
+    {
+        var aReg = Reg(new Register());
+        var cReg = Reg(new Register());
+
+        var bReg = Reg(new Register());
+        var dReg = Reg(new Register());
+
+        var aNodeOne = new SingleExitNode(null, new List<CodeTreeNode> { aReg.Read() });
+        var aNodeTwo = new SingleExitNode(aNodeOne, new List<CodeTreeNode> { cReg.Read() });
+        var aNodeThree = new SingleExitNode(aNodeTwo, new List<CodeTreeNode> { aReg.Read() });
+        aNodeOne.NextTree = aNodeThree;
+
+        var bNodeOne = new SingleExitNode(null, new List<CodeTreeNode> { bReg.Read() });
+        var bNodeTwo = new SingleExitNode(bNodeOne, new List<CodeTreeNode> { dReg.Read() });
+        var bNodeThree = new SingleExitNode(bNodeTwo, new List<CodeTreeNode> { bReg.Read() });
+        bNodeOne.NextTree = bNodeThree;
+
+        // Assert.Equal causes stack overflow (on failure) due to default ToString implementation
+        var comparer = new CfgIsomorphismComparer();
+        Assert.True(comparer.Equals(aNodeThree, bNodeThree));
+    }
+
+    [Fact]
+    public void DifferentCycles()
+    {
+        var aReg = Reg(new Register());
+        var cReg = Reg(new Register());
+
+        var bReg = Reg(new Register());
+        var dReg = Reg(new Register());
+
+        var aNodeOne = new SingleExitNode(null, new List<CodeTreeNode> { aReg.Read() });
+        var aNodeTwo = new SingleExitNode(aNodeOne, new List<CodeTreeNode> { cReg.Read() });
+        var aNodeThree = new SingleExitNode(aNodeTwo, new List<CodeTreeNode> { aReg.Read() });
+        aNodeOne.NextTree = aNodeThree;
+
+        var bNodeOne = new SingleExitNode(null, new List<CodeTreeNode> { bReg.Read() });
+        var bNodeTwo = new SingleExitNode(bNodeOne, new List<CodeTreeNode> { dReg.Read() });
+        var bNodeThree = new SingleExitNode(bNodeTwo, new List<CodeTreeNode> { bReg.Read() });
+        bNodeOne.NextTree = bNodeTwo;
+
+        var comparer = new CfgIsomorphismComparer();
+        Assert.False(comparer.Equals(aNodeThree, bNodeThree));
+    }
+}
