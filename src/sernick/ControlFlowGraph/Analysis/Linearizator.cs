139422e769a978e7450e32418b497807232ca2e1
Władysław Raczek <wsw.raczek@gmail.com>
2022-12-10 12:29:36 +0100

    More helper methods :stuck_out_tongue:

--- src/sernick/ControlFlowGraph/Analysis/Linearizator.cs
+++ src/sernick/ControlFlowGraph/Analysis/Linearizator.cs
@@ -6,7 +6,7 @@ using CodeTree;
 public sealed class Linearizator
 {
     private readonly InstructionCovering _instructionCovering;
-    private Dictionary<CodeTreeRoot, Label> _visitedRootsLabels;
+    private readonly Dictionary<CodeTreeRoot, Label> _visitedRootsLabels;
 
     public Linearizator(InstructionCovering instructionCovering)
     {
@@ -48,46 +48,52 @@ public sealed class Linearizator
         }
     }
 
-        private IEnumerable<IAsmable> handleSingleExitNode(SingleExitNode node, int depth)
+    private IEnumerable<IAsmable> handleSingleExitNode(SingleExitNode node, int depth)
+    {
+        if (node.NextTree == null)
         {
-            if (node.NextTree == null)
-            {
-                return _instructionCovering.Cover(node, null);
-            }
-
-            var nextDepth = depth + 1;
-            var nextTree = node.NextTree;
-            var nextTreeCover = dfs(nextTree, nextDepth);
-            var labelForNextTree = generateLabel(depth);
-            _visitedRootsLabels.Add(nextTree, labelForNextTree);
-
-            var nodeCover = (List<IAsmable>)_instructionCovering.Cover(node, labelForNextTree);
-            return nodeCover.Append(labelForNextTree).Concat(nextTreeCover);
+            return _instructionCovering.Cover(node, null);
         }
 
-        private IEnumerable<IAsmable> handleConditionalJumpNode(ConditionalJumpNode conditionalNode, int depth)
+        var nextDepth = depth + 1;
+        var (nextTreeLabel, nextTreeCover) = processTree(node.NextTree, nextDepth);
+
+        var nodeCover = (List<IAsmable>)_instructionCovering.Cover(node, nextTreeLabel);
+        return nodeCover.Append(nextTreeLabel).Concat(nextTreeCover);
+    }
+
+    private IEnumerable<IAsmable> handleConditionalJumpNode(ConditionalJumpNode conditionalNode, int depth)
+    {
+        var nextDepth = depth + 1;
+        var trueCaseNode = conditionalNode.TrueCase;
+        if (trueCaseNode == null)
         {
-            var nextDepth = depth + 1;
-            var trueCaseNode = conditionalNode.TrueCase;
-            if (trueCaseNode == null)
-            {
-                throw new Exception("<Linearizator> Node " + conditionalNode + " has TrueCase equal to null, but it should be non-nullable");
-            }
-            var trueCaseLabel = generateLabel(depth);
-            var trueCaseCover = dfs(trueCaseNode, nextDepth);
-            _visitedRootsLabels.Add(trueCaseNode, trueCaseLabel);
-
-            var falseCaseNode = conditionalNode.FalseCase;
-            if (falseCaseNode == null)
-            {
-                throw new Exception("<Linearizator> Node " + conditionalNode + " has TrueCase equal to null, but it should be non-nullable");
-            }
-            var falseCaseLabel = generateLabel(depth);
-            var falseCaseCover = dfs(falseCaseNode, nextDepth);
-            _visitedRootsLabels.Add(falseCaseNode, falseCaseLabel);
-
-            var conditionalNodeCover = (List<IAsmable>)_instructionCovering.Cover(conditionalNode, trueCaseLabel, falseCaseLabel);
-            return conditionalNodeCover.Append(trueCaseLabel).Concat(trueCaseCover).Append(falseCaseLabel).Concat(falseCaseCover);
-
+            throw new Exception("<Linearizator> Node " + conditionalNode + " has TrueCase equal to null, but it should be non-nullable");
         }
+        var (trueCaseLabel, trueCaseCover) = processTree(trueCaseNode, nextDepth);
+
+        var falseCaseNode = conditionalNode.FalseCase;
+        if (falseCaseNode == null)
+        {
+            throw new Exception("<Linearizator> Node " + conditionalNode + " has TrueCase equal to null, but it should be non-nullable");
+        }
+        var (falseCaseLabel, falseCaseCover) = processTree(falseCaseNode, nextDepth);
+
+        var conditionalNodeCover = (List<IAsmable>)_instructionCovering.Cover(conditionalNode, trueCaseLabel, falseCaseLabel);
+        return conditionalNodeCover.Append(trueCaseLabel).Concat(trueCaseCover).Append(falseCaseLabel).Concat(falseCaseCover);
+    }
+
+    private ValueTuple<Label, IEnumerable<IAsmable>> processTree(CodeTreeRoot tree, int depth)
+    {
+        if (_visitedRootsLabels.ContainsKey(tree))
+        {
+            var label = _visitedRootsLabels[tree];
+            var cover = new List<IAsmable>() { label };
+            return (label, cover);
+        }
+        var treeLabel = generateLabel(depth);
+        var treeCover = dfs(tree, depth + 1);
+        _visitedRootsLabels.Add(tree, treeLabel);
+        return ( treeLabel, treeCover );
+    }
 }
