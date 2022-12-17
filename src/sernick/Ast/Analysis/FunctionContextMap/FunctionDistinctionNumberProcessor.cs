ec1662d878bc862951045972ea5b3571c9ebcc29
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-12-17 23:24:37 +0100

    Add second visitor

--- src/sernick/Ast/Analysis/FunctionContextMap/FunctionDistinctionNumberProcessor.cs
+++ src/sernick/Ast/Analysis/FunctionContextMap/FunctionDistinctionNumberProcessor.cs
@@ -9,9 +9,13 @@ public static class FunctionDistinctionNumberProcessor
 
     public static DistinctionNumberProvider Process(AstNode root)
     {
-        return f => null;
-        // var dict = new Dictionary<FunctionDefinition, int?>();
-        // return f => dict[f];
+        var functionChildrenNames = new Dictionary<FunctionDefinition, Multiset<string>>();
+        var preprocessVisitor = new FunctionLabelPreprocessVisitor();
+        root.Accept(preprocessVisitor, new PreprocessVisitorParam(functionChildrenNames));
+        var result = new Dictionary<FunctionDefinition, int?>();
+        var visitor = new FunctionLabelVisitor(functionChildrenNames);
+        root.Accept(visitor, new VisitorParam(result));
+        return f => result[f];
     }
     
     
@@ -37,6 +41,62 @@ public static class FunctionDistinctionNumberProcessor
                     param.FunctionChildrenNames[param.EnclosingFunction] = new Multiset<string>();
                 }
                 param.FunctionChildrenNames[param.EnclosingFunction].Add(node.Name.Name);
+            }
+            foreach (var parameter in node.Parameters)
+            {
+                parameter.Accept(this, param);
+            }
+            node.Body.Accept(this, param with { EnclosingFunction = node });
+            return Unit.I;
+        }
+    }
+
+
+    private record VisitorParam(
+        IDictionary<FunctionDefinition, int?> DistinctionNumbers,
+        Multiset<(FunctionDefinition, string)> ReoccurringFunctions,
+        FunctionDefinition? EnclosingFunction = null)
+    {
+        public VisitorParam(Dictionary<FunctionDefinition, int?> distinctionNumbers) : this(distinctionNumbers, new Multiset<(FunctionDefinition, string)>())
+        {
+        }
+    }
+
+    private sealed class FunctionLabelVisitor : AstVisitor<Unit, VisitorParam>
+    {
+        private IDictionary<FunctionDefinition, Multiset<string>> _childrenNames;
+        public FunctionLabelVisitor(IDictionary<FunctionDefinition, Multiset<string>> childrenNames)
+        {
+            _childrenNames = childrenNames;
+        }
+        
+        protected override Unit VisitAstNode(AstNode node, VisitorParam param)
+        {
+            foreach (var child in node.Children)
+            {
+                child.Accept(this, param);
+            }
+            return Unit.I;
+        }
+
+        public override Unit VisitFunctionDefinition(FunctionDefinition node, VisitorParam param)
+        {
+            if (param.EnclosingFunction is null)
+            {
+                param.DistinctionNumbers[node] = null;
+            }
+            else
+            {
+                if (_childrenNames[param.EnclosingFunction].Get(node.Name.Name) > 1)
+                {
+                    var amount = param.ReoccurringFunctions.Get((param.EnclosingFunction, node.Name.Name));
+                    param.DistinctionNumbers[node] = amount + 1;
+                    param.ReoccurringFunctions.Add((param.EnclosingFunction, node.Name.Name));
+                }
+                else
+                {
+                    param.DistinctionNumbers[node] = null;
+                }
             }
             foreach (var parameter in node.Parameters)
             {
