47c111d46e17388a694702b0f695e1105f206a1a
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-12-17 13:03:18 +0100

    Add caller for read

---
+++ src/sernick/Compiler/Function/Common.cs
@@ -0,0 +1,56 @@
+namespace sernick.Compiler.Function;
+
+using ControlFlowGraph.CodeTree;
+
+public static class Convention
+{
+    public static readonly HardwareRegister[] CallerToSave = {
+        HardwareRegister.R8,
+        HardwareRegister.R9,
+        HardwareRegister.R10,
+        HardwareRegister.R11,
+        HardwareRegister.RAX,
+        HardwareRegister.RCX,
+        HardwareRegister.RDX,
+    };
+
+    public static readonly HardwareRegister[] CalleeToSave = {
+        HardwareRegister.R12,
+        HardwareRegister.R13,
+        HardwareRegister.R14,
+        HardwareRegister.R15,
+        HardwareRegister.RBX,
+        HardwareRegister.RDI,
+        HardwareRegister.RSI,
+    };
+
+    public static readonly HardwareRegister[] ArgumentRegisters = {
+        HardwareRegister.RDI,
+        HardwareRegister.RSI,
+        HardwareRegister.RDX,
+        HardwareRegister.RCX,
+        HardwareRegister.R8,
+        HardwareRegister.R9,
+    };
+
+    public static readonly int REG_ARGS_COUNT = ArgumentRegisters.Count();
+}
+
+public static class Helpers
+{
+    public static List<SingleExitNode> CodeTreeListToSingleExitList(List<CodeTreeNode> trees)
+    {
+        var result = new List<SingleExitNode>();
+        trees.Reverse();
+        SingleExitNode? nextRoot = null;
+        foreach (var tree in trees)
+        {
+            var treeRoot = new SingleExitNode(nextRoot, new List<CodeTreeNode> { tree });
+            result.Add(treeRoot);
+            nextRoot = treeRoot;
+        }
+
+        result.Reverse();
+        return result;
+    }
+}
---
+++ src/sernick/Compiler/Function/ReadCaller.cs
@@ -0,0 +1,69 @@
+namespace sernick.Compiler.Function;
+
+using CodeGeneration;
+using ControlFlowGraph.CodeTree;
+using static Compiler.PlatformConstants;
+using static ControlFlowGraph.CodeTree.CodeTreeExtensions;
+using static Convention;
+using static Helpers;
+
+public sealed class ReadCaller : IFunctionCaller
+{
+    // Not 100 % sure about it
+    private const int FORMAT_STRING = 0x256400;
+    public Label Label { get; } = "scanf";
+
+    public IFunctionCaller.GenerateCallResult GenerateCall(IReadOnlyList<CodeTreeValueNode> arguments)
+    {
+        var operations = new List<CodeTreeNode>();
+
+        // Caller-saved registers
+        var callerSavedMap = new Dictionary<HardwareRegister, Register>(ReferenceEqualityComparer.Instance);
+        foreach (var reg in CallerToSave)
+        {
+            var tempReg = new Register();
+            callerSavedMap[reg] = tempReg;
+            operations.Add(Reg(tempReg).Write(Reg(reg).Read()));
+        }
+
+        Register rsp = HardwareRegister.RSP;
+        var rspRead = Reg(rsp).Read();
+        var pushRsp = Reg(rsp).Write(rspRead - POINTER_SIZE);
+
+        // Allocate slot for input value
+        operations.Add(pushRsp);
+
+        // Arguments:
+        // address
+        operations.Add(pushRsp);
+        operations.Add(Mem(rspRead).Write(rspRead + POINTER_SIZE));
+
+        // format string
+        operations.Add(pushRsp);
+        operations.Add(Mem(rspRead).Write(FORMAT_STRING));
+
+        // Performing actual call (puts return addess on stack and jumps)
+        operations.Add(new FunctionCall(this));
+
+        // Remove arguments from stack (we already returned from call)
+        operations.Add(Reg(rsp).Write(rspRead + POINTER_SIZE * 2));
+
+        // Put value from stack to virtual register
+        var returnValueRegister = new Register();
+        operations.Add(Reg(returnValueRegister).Write(Mem(rspRead).Read()));
+        CodeTreeValueNode returnValueLocation = Reg(returnValueRegister).Read();
+
+        // Free stack output slot
+        operations.Add(Reg(rsp).Write(rspRead + POINTER_SIZE));
+
+        // Retrieve values of caller-saved registers
+        foreach (var reg in CallerToSave)
+        {
+            var tempReg = callerSavedMap[reg];
+            var tempVal = Reg(tempReg).Read();
+            operations.Add(Reg(reg).Write(tempVal));
+        }
+
+        return new IFunctionCaller.GenerateCallResult(CodeTreeListToSingleExitList(operations), returnValueLocation);
+    }
+}
