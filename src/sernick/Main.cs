378629c327898e8b1840a20860ff19df0ebd34b9
Demian Banakh <dembanakh01@gmail.com>
2022-12-17 22:47:11 +0100

    In progress

--- src/sernick/Main.cs
+++ src/sernick/Main.cs
@@ -1,9 +1,83 @@
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using sernick.Compiler;
 using sernick.Diagnostics;
 using sernick.Utility;
 
 [assembly: InternalsVisibleTo("sernickTest")]
+
+const string ASM = @"section .text
+extern printf
+global main
+
+main:
+    push rbp
+    mov rdi, message
+    xor rax, rax
+    call printf
+    pop rbp
+    mov rax, 0
+    ret
+
+section .data
+    message: db 'Hello World', 10, 0";
+
+Console.WriteLine("Assembling:");
+Console.WriteLine(ASM);
+
+await File.WriteAllTextAsync("main.asm", ASM);
+
+(string stderr, string stdout) RunProcess(string cmd, string arguments = "")
+{
+    var process = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = cmd,
+            Arguments = arguments,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+            UseShellExecute = false
+        }
+    };
+
+    process.Start();
+
+    var stdout = process.StandardOutput.ReadToEnd();
+    var stderr = process.StandardError.ReadToEnd();
+    process.WaitForExit();
+
+    return (stderr, stdout);
+}
+
+var (errors, _) = RunProcess("nasm", "-f elf64 -o main.o main.asm");
+
+if (errors.Length > 0)
+{
+    // handle errors
+    Environment.Exit(1);
+}
+
+(errors, _) = RunProcess("gcc", "-no-pie -o main.out main.o");
+
+if (errors.Length > 0)
+{
+    // handle errors
+    Environment.Exit(1);
+}
+
+(errors, var output) = RunProcess("main.out");
+
+if (errors.Length > 0)
+{
+    // handle errors
+    Environment.Exit(1);
+}
+
+Console.WriteLine("Success:");
+Console.WriteLine(output);
 
 // check if filename was provided
 if (args.Length == 0)
