d9cd5126d4d77fafa1a3e200427176b602014603
Krzysztof Ziobro <jerzym27@protonmail.com>
2022-12-17 15:05:13 +0100

    fmt

--- src/sernick/Ast/Analysis/ExternalFunctions.cs
+++ src/sernick/Ast/Analysis/ExternalFunctions.cs
@@ -1,7 +1,7 @@
 namespace sernick.Ast.Analysis;
 
-using sernick.Compiler.Function;
 using sernick.Ast.Nodes;
+using sernick.Compiler.Function;
 using sernick.Input;
 using sernick.Input.String;
 using sernick.Utility;
@@ -10,36 +10,36 @@ public static class ExternalFunctionsInfo
 {
     public record FunctionInfo(FunctionDefinition Definition, IFunctionCaller Caller);
 
-    private static Range<ILocation> placeholderRange = new Range<ILocation>(new StringLocation(0), new StringLocation(0));
+    private static readonly Range<ILocation> placeholderRange = new Range<ILocation>(new StringLocation(0), new StringLocation(0));
 
     public static FunctionInfo[] ExternalFunctions = {
         new FunctionInfo(
             new FunctionDefinition(
-                new Identifier("read", placeholderRange), 
-                new List<FunctionParameterDeclaration>(), 
-                new IntType(), 
+                new Identifier("read", placeholderRange),
+                new List<FunctionParameterDeclaration>(),
+                new IntType(),
                 new CodeBlock(
-                    new ReturnStatement(new IntLiteralValue(0, placeholderRange), placeholderRange), 
+                    new ReturnStatement(new IntLiteralValue(0, placeholderRange), placeholderRange),
                     placeholderRange
                     ),
                 placeholderRange
-                ), 
+                ),
                 new ReadCaller()
         ),
         new FunctionInfo(
             new FunctionDefinition(
-                new Identifier("write", placeholderRange), 
-                new List<FunctionParameterDeclaration> { 
+                new Identifier("write", placeholderRange),
+                new List<FunctionParameterDeclaration> {
                     new FunctionParameterDeclaration(
-                        new Identifier("placeholder", placeholderRange), 
-                        new UnitType(), 
-                        null, 
+                        new Identifier("placeholder", placeholderRange),
+                        new UnitType(),
+                        null,
                         placeholderRange
-                        ) 
-                    }, 
-                new IntType(), 
+                        )
+                    },
+                new IntType(),
                 new CodeBlock(
-                    new ReturnStatement(null, placeholderRange), 
+                    new ReturnStatement(null, placeholderRange),
                     placeholderRange
                     ),
                 placeholderRange
