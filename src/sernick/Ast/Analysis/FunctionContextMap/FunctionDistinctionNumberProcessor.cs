2bc1afc89f53f7a10259fea74a8c1926d5ada43d
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-12-17 23:09:18 +0100

    Add first Visitor

--- src/sernick/Ast/Analysis/FunctionContextMap/FunctionDistinctionNumberProcessor.cs
+++ src/sernick/Ast/Analysis/FunctionContextMap/FunctionDistinctionNumberProcessor.cs
@@ -1,6 +1,7 @@
 ﻿namespace sernick.Ast.Analysis.FunctionContextMap;
 
 using Nodes;
+using Utility;
 
 public static class FunctionDistinctionNumberProcessor
 {
@@ -11,5 +12,38 @@ public static class FunctionDistinctionNumberProcessor
         return f => null;
         // var dict = new Dictionary<FunctionDefinition, int?>();
         // return f => dict[f];
+    }
+    
+    
+    private record PreprocessVisitorParam(IDictionary<FunctionDefinition, Multiset<string>> FunctionChildrenNames,
+        FunctionDefinition? EnclosingFunction = null);
+    private sealed class FunctionLabelPreprocessVisitor : AstVisitor<Unit, PreprocessVisitorParam>
+    {
+        protected override Unit VisitAstNode(AstNode node, PreprocessVisitorParam param)
+        {
+            foreach (var child in node.Children)
+            {
+                child.Accept(this, param);
+            }
+            return Unit.I;
+        }
+
+        public override Unit VisitFunctionDefinition(FunctionDefinition node, PreprocessVisitorParam param)
+        {
+            if (param.EnclosingFunction is not null)
+            {
+                if (!param.FunctionChildrenNames.ContainsKey(param.EnclosingFunction))
+                {
+                    param.FunctionChildrenNames[param.EnclosingFunction] = new Multiset<string>();
+                }
+                param.FunctionChildrenNames[param.EnclosingFunction].Add(node.Name.Name);
+            }
+            foreach (var parameter in node.Parameters)
+            {
+                parameter.Accept(this, param);
+            }
+            node.Body.Accept(this, param with { EnclosingFunction = node });
+            return Unit.I;
+        }
     }
 }
---
+++ src/sernick/Utility/Multiset.cs
@@ -0,0 +1,27 @@
+﻿namespace sernick.Utility;
+
+public class Multiset<K> where K : notnull
+{
+    private readonly Dictionary<K, int> _dict;
+    public Multiset()
+    {
+        _dict = new Dictionary<K, int>();
+    }
+
+    public void Add(K elem)
+    {
+        if (_dict.TryGetValue(elem, out var value))
+        {
+            _dict[elem] = value + 1;
+        }
+        else
+        {
+            _dict[elem] = 1;
+        }
+    }
+
+    public int Get(K elem)
+    {
+        return _dict.TryGetValue(elem, out var value) ? value : 0;
+    }
+}
