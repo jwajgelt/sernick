439a8175202320509191b10806ad799b79365c9c
Demian Banakh <dembanakh01@gmail.com>
2022-12-20 10:41:07 +0100

    Copy examples folder to docker

--- run_in_docker.bat
+++ run_in_docker.bat
@@ -1,12 +1,16 @@
 @ECHO OFF
+
+:: Usage: ./run_in_docker.bat examples/.../xxx.ser
+
 docker rmi sernick-image:latest 2> nul
 docker stop sernick 2> nul
 docker rm sernick 2> nul
 
 cd src\sernick
 dotnet publish -c Release
-docker build -t sernick-image -f Dockerfile .
-docker create --name sernick sernick-image
+cd ..\..
+docker build -t sernick-image -f src/sernick/Dockerfile .
+docker create --name sernick sernick-image %1 --execute
 docker start sernick
 docker wait sernick
 echo:
