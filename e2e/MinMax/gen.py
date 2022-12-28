14a50f6fa9b3679e5c82c0e94e4f12923aa65fcd
Władysław Raczek <wsw.raczek@gmail.com>
2022-12-28 18:10:05 +0100

    Fix tests

--- e2e/MinMax/gen.py
+++ e2e/MinMax/gen.py
@@ -1,14 +1,14 @@
 import random
 
-n = 100 * 1000
+n = 1000 * 1000
 random.seed()
 
 print(n)
 
-data=[str(random.randrange(1, 1000*1000*1000)) for i in range(n)]
+data=[random.randrange(1, 1000*1000*1000) for i in range(n)]
 
 minimal = str(min(data))
 maximal = str(max(data))
 
-print(' '.join(data))
+print(*data)
 print(' '.join([minimal, maximal]))
