23ed6bf2c05cf77b97bb6ac0eb4a3ffcc2051cfd
Juliusz Wajgelt <jwajgelt@gmail.com>
2022-12-30 14:55:51 +0100

    VariableInitialization impleentation for function calls

--- src/sernick/Ast/Analysis/VariableInitialization/VariableInitializationAnalyzer.cs
+++ src/sernick/Ast/Analysis/VariableInitialization/VariableInitializationAnalyzer.cs
@@ -1,19 +1,51 @@
 namespace sernick.Ast.Analysis.VariableInitialization;
 
 using System.Collections.Immutable;
+using CallGraph;
 using Diagnostics;
+using FunctionContextMap;
 using NameResolution;
 using Nodes;
 using Utility;
+using VariableAccess;
 
-public sealed class VariableInitializationAnalyzer
+public static class VariableInitializationAnalyzer
 {
-    public VariableInitializationAnalyzer()
+    public static void Process(
+        FunctionDefinition function,
+        FunctionContextMap functionContextMap,
+        VariableAccessMap variableAccessMap,
+        NameResolutionResult nameResolution,
+        CallGraph callGraph,
+        IDiagnostics diagnostics)
     {
+        var enclosedFunctionsCallGraph = callGraph.ClosureWithinScope(function, functionContextMap);
+        var localVariables = LocalVariableDeclarations.Process(function).ToHashSet();
 
+        // for all enclosed functions, calculate the variable accesses to function's local variables
+        var localVariableAccessMap = enclosedFunctionsCallGraph.Graph.ToDictionary(
+            kv => kv.Key,
+            kv => kv.Value
+                .SelectMany(calledFunction => variableAccessMap[calledFunction])
+                .SelectMany(variableAccess =>
+                    variableAccess.Item1 is VariableDeclaration variableDeclaration
+                        ? variableDeclaration.Enumerate()
+                        : Enumerable.Empty<VariableDeclaration>())
+                .Where(variable => localVariables.Contains(variable))
+                .ToHashSet().AsEnumerable());
+
+        try
+        {
+            function.Accept(new VariableInitializationVisitor(nameResolution, localVariableAccessMap),
+                new VariableInitializationVisitorParam());
+        }
+        catch (VariableInitializationVisitorException e)
+        {
+            diagnostics.Report(e.Error);
+        }
     }
 
-    public abstract class VariableInitializationAnalysisError : IDiagnosticItem
+    private abstract class VariableInitializationAnalysisError : IDiagnosticItem
     {
         public abstract override string ToString();
         public DiagnosticItemSeverity Severity => DiagnosticItemSeverity.Error;
@@ -29,6 +61,20 @@ public sealed class VariableInitializationAnalyzer
         public override string ToString() => $"Use of uninitialized variable {_value.Identifier} at {_value.LocationRange}";
 
         private readonly VariableValue _value;
+    }
+
+    private class UninitializedNonLocalVariableUseError : VariableInitializationAnalysisError
+    {
+        public UninitializedNonLocalVariableUseError(VariableDeclaration variable, FunctionDefinition functionDefinition)
+        {
+            _variable = variable;
+            _functionDefinition = functionDefinition;
+        }
+
+        public override string ToString() => $"Use of potentially uninitialized variable {_variable} inside {_functionDefinition}";
+
+        private readonly VariableDeclaration _variable;
+        private readonly FunctionDefinition _functionDefinition;
     }
 
     private class MultipleConstAssignmentError : VariableInitializationAnalysisError
@@ -71,20 +117,41 @@ public sealed class VariableInitializationAnalyzer
         }
     }
 
-    private sealed class
-            VariableInitializationVisitor : AstVisitor<VariableInitializationVisitorResult,
-                VariableInitializationVisitorParam>
+    private sealed class VariableInitializationVisitor
+        : AstVisitor<VariableInitializationVisitorResult, VariableInitializationVisitorParam>
     {
-        public VariableInitializationVisitor(NameResolutionResult nameResolution)
+        public VariableInitializationVisitor(
+            NameResolutionResult nameResolution,
+            Dictionary<FunctionDefinition, IEnumerable<VariableDeclaration>> localVariableAccessMap)
         {
             _nameResolution = nameResolution;
+            _localVariableAccessMap = localVariableAccessMap;
         }
 
         protected override VariableInitializationVisitorResult VisitAstNode(AstNode node, VariableInitializationVisitorParam param)
         {
-            // walk through children
-            // when a child returns 'diverges', add further expressions' results as "maybe initialized"
-            throw new NotImplementedException();
+            var initializedVariables = ImmutableHashSet<VariableDeclaration>.Empty;
+            var maybeInitializedVariables = ImmutableHashSet<VariableDeclaration>.Empty;
+            var diverges = false;
+
+            foreach (var child in node.Children)
+            {
+                var childResult = VisitAstNode(child, new VariableInitializationVisitorParam(initializedVariables, maybeInitializedVariables));
+                if (diverges)
+                {
+                    initializedVariables = initializedVariables.Union(childResult.initializedVariables);
+                }
+                else
+                {
+                    maybeInitializedVariables = maybeInitializedVariables.Union(childResult.initializedVariables);
+                }
+
+                maybeInitializedVariables = maybeInitializedVariables.Union(childResult.maybeInitializedVariables);
+                maybeInitializedVariables = maybeInitializedVariables.Union(initializedVariables);
+                diverges = diverges || childResult.diverges;
+            }
+
+            return new VariableInitializationVisitorResult(initializedVariables, maybeInitializedVariables, diverges);
         }
 
         protected override VariableInitializationVisitorResult VisitDeclaration(Declaration node, VariableInitializationVisitorParam param)
@@ -124,7 +191,25 @@ public sealed class VariableInitializationAnalyzer
 
         public override VariableInitializationVisitorResult VisitFunctionCall(FunctionCall node, VariableInitializationVisitorParam param)
         {
-            throw new NotImplementedException("TODO: check accessed variables of function and throw if invalid");
+            var paramVisitResult = VisitAstNode(node, param);
+            var calledFunction = _nameResolution.CalledFunctionDeclarations[node];
+
+            if (!_localVariableAccessMap.ContainsKey(calledFunction))
+            {
+                return paramVisitResult;
+            }
+
+            // calling a local function
+            foreach (var variable in _localVariableAccessMap[calledFunction])
+            {
+                if (!paramVisitResult.initializedVariables.Contains(variable) &&
+                    !param.initializedVariables.Contains(variable))
+                {
+                    throw new VariableInitializationVisitorException(new UninitializedNonLocalVariableUseError(variable, calledFunction));
+                }
+            }
+
+            return paramVisitResult;
         }
 
         public override VariableInitializationVisitorResult VisitContinueStatement(ContinueStatement node,
@@ -158,7 +243,7 @@ public sealed class VariableInitializationAnalyzer
         {
             var assignedVariable = _nameResolution.AssignedVariableDeclarations[node];
 
-            if (assignedVariable.IsConst && param.initializedVariables.Contains(assignedVariable))
+            if (assignedVariable.IsConst && param.maybeInitializedVariables.Contains(assignedVariable))
             {
                 // check if there's multiple assignments to const
                 throw new VariableInitializationVisitorException(new MultipleConstAssignmentError(node));
@@ -185,5 +270,6 @@ public sealed class VariableInitializationAnalyzer
         }
 
         private readonly NameResolutionResult _nameResolution;
+        private readonly Dictionary<FunctionDefinition, IEnumerable<VariableDeclaration>> _localVariableAccessMap;
     }
 }
---
+++ test/sernickTest/Ast/Analysis/VariableInitialization/VariableInitializationAnalyzerTest.cs
@@ -0,0 +1,10 @@
+namespace sernickTest.Ast.Analysis.VariableInitialization;
+
+public class VariableInitializationAnalyzerTest
+{
+    [Fact]
+    public void ReportsErrorOnMultipleConstDeclaration()
+    {
+        
+    }
+}
