440ca800f2e4debdfc8b4997d695b806c50bb5bb
Władysław Raczek <wsw.raczek@gmail.com>
2023-01-01 13:21:06 +0100

    Use logger in testHelpers

--- e2e/testHelpers.py
+++ e2e/testHelpers.py
@@ -3,7 +3,7 @@ import subprocess
 import re
 import filecmp
 from typing import List
-
+import logging
 
 INPUT_DIR = r'Input'
 OUTPUT_DIR = r'Output'
@@ -29,27 +29,27 @@ def get_compiled_files(dir)-> List[str]:
     compiled_files = []
     for f in sernick_files:
         pass # TODO call subprocess with something like nasm
-    print('Compiled the following files: {}'.format(compiled_files))
+    logging.INFO('Compiled the following files: {}'.format(compiled_files))
     return compiled_files
 
 def get_files(directory: str) -> List[str]:
     return [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
 def prepare_test_data(test_directory: str) -> bool:
-    print("Preparing test data for folder " + test_directory)
+    logging.INFO("Preparing test data for folder " + test_directory)
     all_files_in_dir = get_files(test_directory)
     directory_contains_python_generator = True if 'gen.py' in all_files_in_dir else False
     if directory_contains_python_generator:
-        print("Running gen.py in " + test_directory)
+        logging.INFO("Running gen.py in " + test_directory)
         subprocess.run(['/usr/bin/python3', 'gen.py'], cwd=test_directory)
     else:
-        print("No gen.py, assuming tests are already there...")
+        logging.INFO("No gen.py, assuming tests are already there...")
     expected_dir_path = os.path.join(test_directory, EXPECTED_DIR)
     if not os.path.exists(expected_dir_path):
         os.makedirs(expected_dir_path)
 
 def run_file(binary_file_path: str, test_directory: str) -> None:
-    print("Running {}".format(binary_file_path))
+    logging.INFO("Running {}".format(binary_file_path))
 
     input_dir = os.path.join(test_directory, INPUT_DIR)
     for input_file in get_files(input_dir):
@@ -69,9 +69,9 @@ def check_output(test_directory: str) -> None:
     for actual, expected in zip(output_files, expected_files):
         files_equal = filecmp.cmp(actual, expected, shallow=False)
         if files_equal:
-            print("Correct answer on " + expected + " ! ✅")
+            logging.INFO("Correct answer on " + expected + " ! ✅")
         else:
-            print("Bad answer on " + expected + " ! ❌")
+            logging.INFO("Bad answer on " + expected + " ! ❌")
 
 def has_tests(test_directory: str)->bool:
     input_dir = os.path.join(test_directory, INPUT_DIR)
@@ -79,6 +79,6 @@ def has_tests(test_directory: str)->bool:
     expected_dir = os.path.join(test_directory, EXPECTED_DIR)
     for d in [input_dir, output_dir, expected_dir]:
         if not os.path.exists(d):
-            print("No directory named {} for {}".format(d, test_directory))
+            logging.DEBUG("No directory named {} for {}".format(d, test_directory))
             return False
     return True
--- e2e/tester.py
+++ e2e/tester.py
@@ -6,6 +6,7 @@ import filecmp
 from enum import Enum
 import logging
 from typing import List
+from loglevel import LOG_LEVEL
 from testHelpers import get_files, find_test_folders, get_compiled_files, has_tests, INPUT_DIR, OUTPUT_DIR, EXPECTED_DIR, TEST_DIR_REGEX
 
 # TODO refactor for more readable code
@@ -34,14 +35,18 @@ def prepare_test_data(test_directory: str) -> TestingLevel:
         return TestingLevel.ONLY_COMPILE
 
 
-def run_file(binary_file_path: str, test_directory: str) -> None:
-    logging.debug("Running {}".format(binary_file_path))
+def run_file_on_input(binary_file_path: str, test_dir_path: str) -> None:
+    logging.debug("Running a binary file {}".format(binary_file_path))
 
-    input_dir = os.path.join(test_directory, INPUT_DIR)
-    for input_file in get_files(input_dir):
-        output_file_name=os.path.join(test_directory, OUTPUT_DIR, os.path.splitext(os.path.basename(input_file))[0])+'.out'
-        input_fd = open(input_file, 'r')
-        output_fd = open(output_file_name, 'w')
+    input_dir_path = os.path.join(test_dir_path, INPUT_DIR)
+    output_dir_path = os.path.join(test_dir_path, OUTPUT_DIR)
+    for input_file_path in get_files(input_dir_path):
+        input_file_basename_no_extension = os.path.splitext(os.path.basename(input_file_path))[0]
+        output_file_path=os.path.join(output_dir_path, input_file_basename_no_extension) + '.out'
+
+        input_fd = open(input_file_path, 'r')
+        output_fd = open(output_file_path, 'w')
+
         p = subprocess.Popen([binary_file_path], stdin=input_fd, text=True,stdout=output_fd)
         p.wait()
 
@@ -53,8 +58,8 @@ def check_output(test_directory: str) -> None:
     expected_files = get_files(expected_dir)
 
     for actual, expected in zip(output_files, expected_files):
-        files_equal = filecmp.cmp(actual, expected, shallow=False)
-        if files_equal:
+        are_equal = filecmp.cmp(actual, expected, shallow=False)
+        if are_equal:
             logging.info("Correct answer on " + expected + " ! ✅")
         else:
             logging.info("Bad answer on " + expected + " ! ❌")
@@ -71,7 +76,7 @@ def __TEST_FIND_TEST_FOLDERS() -> str:
 def run_files(compiled_files: List[str], test_directory: str)->None:
     for binary_file in compiled_files:
         try:
-            run_file(binary_file_path=binary_file, test_directory=test_directory)
+            run_file_on_input(binary_file_path=binary_file, test_dir_path=test_directory)
             check_output(test_directory)
         except Exception:
             logging.error("Unknown exception occured, proceeding")
@@ -91,5 +96,5 @@ def test():
             run_files(compiled_files=compiled_files, test_directory=test_directory)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=LOG_LEVEL)
     test()
