6fe16380acbee193b7e5ced78c54b5c30a300327
Demian Banakh <dembanakh01@gmail.com>
2023-01-03 11:33:17 +0100

    Improve src/sernick/Dockerfile

--- run_in_docker.bat
+++ run_in_docker.bat
@@ -2,16 +2,12 @@
 
 :: Usage: ./run_in_docker.bat examples/.../xxx.ser
 
+set currentdir=%~dp0
+
 docker rmi sernick-image:latest 2> nul
-docker stop sernick 2> nul
-docker rm sernick 2> nul
 
 cd src\sernick
 dotnet publish -c Release
 cd ..\..
 docker build -t sernick-image -f src/sernick/Dockerfile .
-docker create --name sernick sernick-image %1 --execute
-docker start sernick
-docker wait sernick
-echo:
-docker logs sernick
+docker run -v %currentdir%examples:/sernick/examples --rm sernick-image %1 --execute
