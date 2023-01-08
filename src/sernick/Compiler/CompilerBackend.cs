0621632f10140fa9880a15cd6f0b562bb80f6a24
Juliusz Wajgelt <jwajgelt@gmail.com>
2023-01-08 18:07:06 +0100

    remove `mov reg, reg` from the generated asm

--- src/sernick/Compiler/CompilerBackend.cs
+++ src/sernick/Compiler/CompilerBackend.cs
@@ -60,26 +60,16 @@ public static class CompilerBackend
                     {
                         regAllocation = spilledRegAllocator.Process(interferenceGraph, copyGraph);
                         (asm, completeRegAllocation) = spillsRegAllocator.Process(asm, functionContextMap[funcDef], regAllocation);
-                        foreach (var asmable in asm)
-                        {
-                            try
-                            {
-                                asmable.ToAsm(completeRegAllocation);
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(asmable);
-                                Console.WriteLine(e);
-                                throw;
-                            }
-                        }
                     }
                     else
                     {
                         completeRegAllocation = regAllocation!;
                     }
 
-                    return asm.Select(asmable => asmable.ToAsm(completeRegAllocation));
+                    return asm
+                        // filter out `mov reg, reg` instructions
+                        .Where(asmable => !asmable.IsNoop(completeRegAllocation))
+                        .Select(asmable => asmable.ToAsm(completeRegAllocation));
                 }))
             .Append(displayTable.ToAsm(ImmutableDictionary<Register, HardwareRegister>.Empty));
 
