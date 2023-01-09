cca5283112d825eb023a3176142937fd8e76673b
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2023-01-09 18:22:14 +0100

    Update NameResolutionError messages, uncomment test

--- test/sernickTest/Compiler/CompilerFrontendTest.cs
+++ test/sernickTest/Compiler/CompilerFrontendTest.cs
@@ -161,18 +161,17 @@ public class CompilerFrontendTest
             }),
             
             // code-blocks
-            // TODO: uncomment after fixing name resolution 
-            // ("code-blocks", "access_outside_braces", new IDiagnosticItem[]
-            // {
-            //     new UndeclaredIdentifierError
-            //     (
-            //         new Identifier("x", (FileUtility.LocationAt(5, 1), FileUtility.LocationAt(5, 2)))
-            //     ),
-            //     new UndeclaredIdentifierError
-            //     (
-            //         new Identifier("x", (FileUtility.LocationAt(5, 5), FileUtility.LocationAt(5, 6)))
-            //     )
-            // }),
+            ("code-blocks", "access_outside_braces", new IDiagnosticItem[]
+            {
+                new UndeclaredIdentifierError
+                (
+                    new Identifier("x", (FileUtility.LocationAt(5, 1), FileUtility.LocationAt(5, 2)))
+                ),
+                new UndeclaredIdentifierError
+                (
+                    new Identifier("x", (FileUtility.LocationAt(5, 5), FileUtility.LocationAt(5, 6)))
+                )
+            }),
             ("code-blocks", "mixed_brackets", new IDiagnosticItem[]
             {
                 new SyntaxError<Symbol>
