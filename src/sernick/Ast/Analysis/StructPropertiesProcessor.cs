04c1b5a5938297fca53864dd5ad927afb192e202
Szymon Salabura <ssalabura@protonmail.com>
2023-01-12 17:36:47 +0100

    Add StructPropertiesProcessor template

---
+++ src/sernick/Ast/Analysis/StructPropertiesProcessor.cs
@@ -0,0 +1,33 @@
+namespace sernick.Ast.Analysis.CallGraph;
+
+using NameResolution;
+using Nodes;
+
+public record struct StructProperties(
+    IReadOnlyDictionary<StructDeclaration, int> StructSizes,
+    IReadOnlyDictionary<FieldDeclaration, int> FieldOffsets
+)
+{ };
+
+public static class StructPropertiesProcessor
+{
+    public static StructProperties Process(AstNode ast, NameResolutionResult nameResolution)
+    {
+        throw new NotImplementedException();
+    }
+
+    private sealed class StructPropertiesVisitor : AstVisitor<StructProperties, StructDeclaration?>
+    {
+        private readonly IReadOnlyDictionary<Identifier, StructDeclaration> _structDeclarations;
+
+        public StructPropertiesVisitor(IReadOnlyDictionary<Identifier, StructDeclaration> structDeclarations)
+        {
+            _structDeclarations = structDeclarations;
+        }
+
+        protected override StructProperties VisitAstNode(AstNode node, StructDeclaration? parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
