df714281142a727e692e3aada30e2fd257dc4840
Juliusz Wajgelt <jwajgelt@gmail.com>
2023-01-15 15:50:33 +0100

    pull out helpers into separate file

--- src/sernick/Ast/Analysis/ControlFlowGraph/ControlFlowAnalyzer.cs
+++ src/sernick/Ast/Analysis/ControlFlowGraph/ControlFlowAnalyzer.cs
@@ -11,6 +11,7 @@ using TypeChecking;
 using Utility;
 using VariableAccess;
 using static Compiler.PlatformConstants;
+using static ControlFlowAnalysisHelpers;
 using FunctionCall = Nodes.FunctionCall;
 
 public static class ControlFlowAnalyzer
@@ -126,123 +127,6 @@ public static class ControlFlowAnalyzer
         IValueLocation? ReturnResultVariable, // variable in which the return result should be stored
         bool IsCondition
     );
-
-    private interface IValueLocation
-    {
-        public IEnumerable<CodeTreeNode> GenerateValueWrite(CodeTreeValueNode value);
-
-        public CodeTreeValueNode GenerateValueRead();
-    }
-
-    private interface IStructValueLocation : IValueLocation
-    {
-        public IValueLocation GetPrimitiveField(int fieldOffset);
-        public IStructValueLocation GetField(int fieldOffset);
-    }
-
-    private class VariableValueLocation : IValueLocation
-    {
-        private readonly IFunctionContext _functionContext;
-        private readonly IFunctionVariable _variable;
-
-        public VariableValueLocation(IFunctionContext functionContext, IFunctionVariable variable)
-        {
-            _functionContext = functionContext;
-            _variable = variable;
-        }
-
-        public IEnumerable<CodeTreeNode> GenerateValueWrite(CodeTreeValueNode value)
-        {
-            return _functionContext.GenerateVariableWrite(_variable, value).Enumerate();
-        }
-
-        public CodeTreeValueNode GenerateValueRead()
-        {
-            return _functionContext.GenerateVariableRead(_variable);
-        }
-    }
-
-    private class StructValueLocation : IStructValueLocation
-    {
-        private class PrimitiveFieldLocation : IValueLocation
-        {
-            private readonly int _offset;
-            private readonly IFunctionContext _functionContext;
-            private readonly IFunctionVariable _temp;
-
-            public PrimitiveFieldLocation(IFunctionContext functionContext, IFunctionVariable temp, int offset)
-            {
-                _offset = offset;
-                _functionContext = functionContext;
-                _temp = temp;
-            }
-
-            public IEnumerable<CodeTreeNode> GenerateValueWrite(CodeTreeValueNode value)
-            {
-                return CodeTreeExtensions.Mem(StackLocationAddress).Write(value).Enumerate();
-            }
-
-            public CodeTreeValueNode GenerateValueRead()
-            {
-                return CodeTreeExtensions.Mem(StackLocationAddress).Read();
-            }
-
-            private CodeTreeValueNode StackLocationAddress => _functionContext.GenerateVariableRead(_temp) + _offset;
-        }
-
-        private readonly IFunctionContext _functionContext;
-        private readonly int _size;
-        private readonly int _offset;
-        private readonly IFunctionVariable _temp;
-
-        public StructValueLocation(IFunctionContext functionContext, IFunctionVariable temp, int size)
-        {
-            _functionContext = functionContext;
-            _size = size;
-            _offset = 0;
-            _temp = temp;
-        }
-
-        private StructValueLocation(IFunctionContext functionContext, IFunctionVariable temp, int size, int offset)
-        {
-            _functionContext = functionContext;
-            _temp = temp;
-            _size = size;
-            _offset = offset;
-        }
-
-        public IValueLocation GetPrimitiveField(int fieldOffset)
-        {
-            if (fieldOffset >= _size)
-            {
-                throw new NotSupportedException();
-            }
-
-            return new PrimitiveFieldLocation(_functionContext, _temp, _offset + fieldOffset);
-        }
-
-        public IStructValueLocation GetField(int fieldOffset)
-        {
-            if (fieldOffset > _size)
-            {
-                throw new NotSupportedException();
-            }
-
-            return new StructValueLocation(_functionContext, _temp, _size - fieldOffset, _offset + fieldOffset);
-        }
-
-        public IEnumerable<CodeTreeNode> GenerateValueWrite(CodeTreeValueNode value)
-        {
-            return StructHelper.GenerateStructCopy(StackLocationAddress, value, _size);
-        }
-
-        public CodeTreeValueNode GenerateValueRead()
-        {
-            return StackLocationAddress;
-        }
-
-        private CodeTreeValueNode StackLocationAddress => _functionContext.GenerateVariableRead(_temp) + _offset;
-    }
 
     private class TemporaryLocalVariableFactory
     {
